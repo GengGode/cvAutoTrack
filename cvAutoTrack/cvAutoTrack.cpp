67de0694ff3e509639d7142aded23d6b330240eb
GengGode <GengGode@outlook.com>
2021-06-14 19:41:54 +0800

    添加项目文件。

---
+++ cvAutoTrack/cvAutoTrack.cpp
@@ -0,0 +1,22 @@
+﻿// cvAutoTrack.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "cvAutoTrack.h"
+
+
+// 这是导出变量的一个示例
+CVAUTOTRACK_API int ncvAutoTrack=0;
+
+// 这是导出函数的一个示例。
+CVAUTOTRACK_API int fncvAutoTrack(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+CcvAutoTrack::CcvAutoTrack()
+{
+    return;
+}
---
+++ cvAutoTrack/cvAutoTrack.h
@@ -0,0 +1,22 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 CVAUTOTRACK_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// CVAUTOTRACK_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef CVAUTOTRACK_EXPORTS
+#define CVAUTOTRACK_API __declspec(dllexport)
+#else
+#define CVAUTOTRACK_API __declspec(dllimport)
+#endif
+
+// 此类是从 dll 导出的
+class CVAUTOTRACK_API CcvAutoTrack {
+public:
+	CcvAutoTrack(void);
+	// TODO: 在此处添加方法。
+};
+
+extern CVAUTOTRACK_API int ncvAutoTrack;
+
+CVAUTOTRACK_API int fncvAutoTrack(void);
