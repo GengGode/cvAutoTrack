ea33b948ca310dbecad960d0d2182bc59f6450d7
GengGode <GengGode@outlook.com>
2021-06-18 03:18:45 +0800

    添加了GetUID的实现，去除了不必要的接口

--- cvAutoTrack/resource.h
+++ cvAutoTrack/resource.h
@@ -2,10 +2,19 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 cvAutoTrack.rc 使用
 //
-#define IDB_BITMAP2                     102
-#define IDB_BITMAP1                     103
-#define IDB_BITMAP3                     104
-#define IDB_BITMAP4                     105
+#define IDB_BITMAP1                     102
+#define IDB_BITMAP2                     103
+#define IDB_BITMAP_UID_                 104
+#define IDB_BITMAP_UID0                 105
+#define IDB_BITMAP_UID1                 106
+#define IDB_BITMAP_UID2                 107
+#define IDB_BITMAP_UID3                 108
+#define IDB_BITMAP_UID4                 109
+#define IDB_BITMAP_UID5                 110
+#define IDB_BITMAP_UID6                 111
+#define IDB_BITMAP_UID7                 112
+#define IDB_BITMAP_UID8                 113
+#define IDB_BITMAP_UID9                 114
 
 // Next default values for new objects
 // 
