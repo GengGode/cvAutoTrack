173d3c2476e8a6b35f4b42f36390f02c1cbf8f3e
Gode King <GengGode@outlook.com>
2022-09-05 07:57:45 +0800

    refactor.重构good_matches部分

--- cvAutoTrack/src/AutoTrack.h
+++ cvAutoTrack/src/AutoTrack.h
@@ -267,6 +267,8 @@ private:
 	bool isStarVisible = false;
 
 private:
+	double check_match_paimon_params = 0.9;
+private:
 	// GPU设备数量
 	int gpuDeviceNumber = 0;
 	// 使能GPU设备id
@@ -299,7 +301,50 @@ private:
 	bool getUIDRefMat();
 	bool getAvatarRefMat();
 
-
 private:
-
+	bool clear_error_logs();
+private:
+	bool check_paimon(cv::Rect &paimon_rect);
+	//bool check_minimap(cv::Rect minimap_rect);	
+	//bool check_uid(cv::Rect uid_rect);
+	//bool check_avatar(cv::Rect avatar_rect);
+	//bool check_star(cv::Rect star_rect);
 };
+
+
+inline void draw_good_matches(cv::Mat& img_scene, std::vector<cv::KeyPoint> keypoint_scene, cv::Mat& img_object, std::vector<cv::KeyPoint> keypoint_object, std::vector<cv::DMatch>& good_matches)
+{
+	cv::Mat img_matches, imgmap, imgminmap;
+	drawKeypoints(img_scene, keypoint_scene, imgmap, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	drawKeypoints(img_object, keypoint_object, imgminmap, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	drawMatches(img_object, keypoint_object, img_scene, keypoint_scene, good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+}
+
+inline void calc_good_matches(cv::Mat& img_scene, std::vector<cv::KeyPoint> keypoint_scene, cv::Mat& img_object, std::vector<cv::KeyPoint> keypoint_object, std::vector<std::vector<cv::DMatch>>& KNN_m, double ratio_thresh, double mapScale, std::vector<double>& lisx, std::vector<double>& lisy, double& sumx, double& sumy)
+{
+#ifdef _DEBUG
+	std::vector<cv::DMatch> good_matches;
+#endif
+	for (size_t i = 0; i < KNN_m.size(); i++)
+	{
+		if (KNN_m[i][0].distance < ratio_thresh * KNN_m[i][1].distance)
+		{
+#ifdef _DEBUG
+			good_matches.push_back(KNN_m[i][0]);
+#endif
+			if (KNN_m[i][0].queryIdx >= keypoint_object.size())
+			{
+				continue;
+			}
+			lisx.push_back(((img_object.cols / 2.0 - keypoint_object[KNN_m[i][0].queryIdx].pt.x) * mapScale + keypoint_scene[KNN_m[i][0].trainIdx].pt.x));
+			lisy.push_back(((img_object.rows / 2.0 - keypoint_object[KNN_m[i][0].queryIdx].pt.y) * mapScale + keypoint_scene[KNN_m[i][0].trainIdx].pt.y));
+			sumx += lisx.back();
+			sumy += lisy.back();
+		}
+	}
+#ifdef _DEBUG
+	draw_good_matches(img_scene, keypoint_scene, img_object, keypoint_object, good_matches);
+#endif
+}
+
