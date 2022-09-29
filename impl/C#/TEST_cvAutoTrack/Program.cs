11bbc1849716735253a3a559e7542628b09d2e9b
Gode King <GengGode@outlook.com>
2022-09-30 00:33:53 +0800

    test.添加C#调用样例

---
+++ impl/C#/TEST_cvAutoTrack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace TEST_cvAutoTrack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // 获取版本号
+            byte[] versionBuff = new byte[1024];
+            string version = "       ";
+            
+            //cvAutoTrack.cvAutoTrack.verison(versionBuff);
+            //string version = Encoding.UTF8.GetString(versionBuff);
+            cvAutoTrack.cvAutoTrack.verison(ref version);
+            
+            Console.WriteLine("version: " + version);
+            
+
+
+            // 申请内存
+            byte[] jsonBuffer = new byte[1024];
+
+            // 调用 cvAutoTrack.dll
+            bool ret = cvAutoTrack.cvAutoTrack.GetStarJson(ref jsonBuffer);
+
+            // 输出结果
+            Console.WriteLine("ret = " + ret);
+            Console.WriteLine("jsonBuffer = " + Encoding.UTF8.GetString(jsonBuffer));
+
+            Console.ReadKey();
+            
+        }
+    }
+}
