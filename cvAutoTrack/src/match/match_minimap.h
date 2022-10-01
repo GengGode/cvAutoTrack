e7774dcbb80235267c6fe8bc55c45fb05fb98879
Gode King <GengGode@outlook.com>
2022-10-02 03:23:06 +0800

    style.格式化

--- cvAutoTrack/src/match/match_minimap.h
+++ cvAutoTrack/src/match/match_minimap.h
@@ -125,17 +125,19 @@ struct GenshinMinimap
 
 	GenshinMinimapConfig config;
 };
+namespace TianLi::Match
+{
+	GenshinHandle func_get_handle(HWND& in);
 
-GenshinHandle func_get_handle(HWND& in);
+	void get_genshin_handle(GenshinHandle& genshin_handle);
 
-void get_genshin_handle(GenshinHandle& genshin_handle);
+	void update_genshin_handle(const HWND& old_handle, GenshinHandle& out_genshin_handle);
 
-void update_genshin_handle(const HWND& old_handle, GenshinHandle& out_genshin_handle);
+	void get_genshin_screen(const GenshinHandle& genshin_handle, GenshinScreen& out_genshin_screen);
 
-void get_genshin_screen(const GenshinHandle& genshin_handle, GenshinScreen& out_genshin_screen);
+	bool check_paimon(const GenshinScreen& genshin_screen, GenshinPaimon& out_genshin_paimon);
 
-void check_paimon(const GenshinScreen& genshin_screen, GenshinPaimon& out_genshin_paimon);
+	bool match_minimap_cailb(const GenshinScreen& genshin_screen, GenshinMinimapCailb& out_genshin_minimap_cailb);
 
-void match_minimap_cailb(const GenshinScreen& genshin_screen, GenshinMinimapCailb& out_genshin_minimap_cailb);
-
-void splite_minimap(const GenshinScreen& genshin_screen, GenshinMinimap& out_genshin_minimap);
+	bool splite_minimap(const GenshinScreen& genshin_screen, GenshinMinimap& out_genshin_minimap);
+}
