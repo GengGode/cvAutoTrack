b8829d81ea9b91b6df8590929bf4f1a06aa26bce
Gode King <GengGode@outlook.com>
2022-10-02 00:36:23 +0800

    refactor.重写了resources为单例类

--- cvAutoTrack/src/resource.h
+++ cvAutoTrack/src/resource.h
@@ -5,27 +5,28 @@
 #define IDB_BITMAP_STAR                 101
 #define IDB_BITMAP_PAIMON               102
 
-#define IDB_BITMAP_UID_                 104
-#define IDB_BITMAP_UID0                 105
-#define IDB_BITMAP_UID1                 106
-#define IDB_BITMAP_UID2                 107
-#define IDB_BITMAP_UID3                 108
-#define IDB_BITMAP_UID4                 109
-#define IDB_BITMAP_UID5                 110
-#define IDB_BITMAP_UID6                 111
-#define IDB_BITMAP_UID7                 112
-#define IDB_BITMAP_UID8                 113
-#define IDB_BITMAP_UID9                 114
+#define IDB_BITMAP_UID0                 110
+#define IDB_BITMAP_UID1                 111
+#define IDB_BITMAP_UID2                 112
+#define IDB_BITMAP_UID3                 113
+#define IDB_BITMAP_UID4                 114
+#define IDB_BITMAP_UID5                 115
+#define IDB_BITMAP_UID6                 116
+#define IDB_BITMAP_UID7                 117
+#define IDB_BITMAP_UID8                 118
+#define IDB_BITMAP_UID9                 119
+#define IDB_BITMAP_UID_                 120
 
-#define IDB_PNG_GIMAP                   115
+#define IDB_PNG_GIMAP                   130
+#define IDB_PNG_MINIMAP_CAILB           131
 
-#define IDR_XML_GIMAP_COMPUTE           130
+#define IDR_XML_GIMAP_COMPUTE           140
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
