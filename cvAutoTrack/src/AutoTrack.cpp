4b9c8fd75153f0e43c0af4763ceb5cd37625d0e3
Gode King <GengGode@outlook.com>
2022-10-01 16:20:09 +0800

    feat.适配有标题情况下，取客户端区域

--- cvAutoTrack/src/AutoTrack.cpp
+++ cvAutoTrack/src/AutoTrack.cpp
@@ -2432,84 +2432,20 @@ bool AutoTrack::getGengshinImpactScale()
 	return true;
 }
 
-namespace bitblt
-{
-
-	//bool AutoTrack::getGengshinImpactScreen()
-	//{
-	//	static HBITMAP	hBmp;
-	//	BITMAP bmp;
-
-	//	DeleteObject(hBmp);
-
-	//	if (giHandle == NULL)
-	//	{
-	//		err = 12;//窗口句柄失效
-	//		return false;
-	//	}
-	//	if (!IsWindow(giHandle))
-	//	{
-	//		err = 12;//窗口句柄失效
-	//		return false;
-	//	}
-	//	//获取目标句柄的窗口大小RECT
-	//	GetWindowRect(giHandle, &giRect);/* 对原神窗口的操作 */
-
-	//	//获取目标句柄的DC
-	//	HDC hScreen = GetDC(giHandle);/* 对原神窗口的操作 */
-	//	HDC hCompDC = CreateCompatibleDC(hScreen);
-
-	//	//获取目标句柄的宽度和高度
-	//	int	nWidth = (int)((screen_scale) * (giRect.right - giRect.left));
-	//	int	nHeight = (int)((screen_scale) * (giRect.bottom - giRect.top));
-
-	//	//创建Bitmap对象
-	//	hBmp = CreateCompatibleBitmap(hScreen, nWidth, nHeight);//得到位图
-
-	//	SelectObject(hCompDC, hBmp); //不写就全黑
-
-	//	BitBlt(hCompDC, 0, 0, nWidth, nHeight, hScreen, 0, 0, SRCCOPY);
-
-	//	////释放对象
-	//	DeleteDC(hScreen);
-	//	DeleteDC(hCompDC);
-
-	//	//类型转换
-	//	//这里获取位图的大小信息,事实上也是兼容DC绘图输出的范围
-	//	GetObject(hBmp, sizeof(BITMAP), &bmp);
-
-	//	int nChannels = bmp.bmBitsPixel == 1 ? 1 : bmp.bmBitsPixel / 8;
-	//	//int depth = bmp.bmBitsPixel == 1 ? 1 : 8;
-
-	//	//mat操作
-	//	giFrame.create(cv::Size(bmp.bmWidth, bmp.bmHeight), CV_MAKETYPE(CV_8U, nChannels));
-
-	//	GetBitmapBits(hBmp, bmp.bmHeight * bmp.bmWidth * nChannels, giFrame.data);
-
-	//	giFrame = giFrame(cv::Rect(giClientRect.left, giClientRect.top, giClientSize.width, giClientSize.height));
-
-
-	//	if (giFrame.empty())
-	//	{
-	//		err = 3;
-	//		return false;
-	//	}
-
-	//	if (giFrame.cols < 480 || giFrame.rows < 360)
-	//	{
-	//		err = 13;
-	//		return false;
-	//	}
-	//	return true;
-	//}
-}
-
 bool AutoTrack::getGengshinImpactScreen()
 {
 	cv::Mat Frame;
 	if (capture->capture(Frame))
 	{
 		giFrame = Frame;
+		if (capture->mode == Capture::Mode_DirectX)
+		{
+			int w = giClientRect.right - giClientRect.left;
+			int h = giClientRect.bottom - giClientRect.top;
+			int x = 0;
+			int y = giFrame.rows - h;
+			giFrame = giFrame(cv::Rect(x, y, w, h));
+		}
 		return true;
 	}
 	else
