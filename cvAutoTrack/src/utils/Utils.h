ae51d3df06e1043dd0cc30d0c9fe5537834c882f
Gode King <GengGode@outlook.com>
2022-10-03 00:46:18 +0800

    refactor.定义声明分离
    draw_good_matches
    calc_good_matches

--- cvAutoTrack/src/utils/Utils.h
+++ cvAutoTrack/src/utils/Utils.h
@@ -16,40 +16,9 @@ namespace TianLi::Utils
 	cv::Point2d TransferUserAxes_Tr(cv::Point2d pos, double x, double y, double scale);
 
 
-	inline void draw_good_matches(cv::Mat& img_scene, std::vector<cv::KeyPoint> keypoint_scene, cv::Mat& img_object, std::vector<cv::KeyPoint> keypoint_object, std::vector<cv::DMatch>& good_matches)
-	{
-		cv::Mat img_matches, imgmap, imgminmap;
-		drawKeypoints(img_scene, keypoint_scene, imgmap, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-		drawKeypoints(img_object, keypoint_object, imgminmap, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-		drawMatches(img_object, keypoint_object, img_scene, keypoint_scene, good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-	}
+	void draw_good_matches(cv::Mat& img_scene, std::vector<cv::KeyPoint> keypoint_scene, cv::Mat& img_object, std::vector<cv::KeyPoint> keypoint_object, std::vector<cv::DMatch>& good_matches);
 
-	inline void calc_good_matches(cv::Mat& img_scene, std::vector<cv::KeyPoint> keypoint_scene, cv::Mat& img_object, std::vector<cv::KeyPoint> keypoint_object, std::vector<std::vector<cv::DMatch>>& KNN_m, double ratio_thresh, double mapScale, std::vector<double>& lisx, std::vector<double>& lisy, double& sumx, double& sumy)
-	{
-#ifdef _DEBUG
-		std::vector<cv::DMatch> good_matches;
-#endif
-		for (size_t i = 0; i < KNN_m.size(); i++)
-		{
-			if (KNN_m[i][0].distance < ratio_thresh * KNN_m[i][1].distance)
-			{
-#ifdef _DEBUG
-				good_matches.push_back(KNN_m[i][0]);
-#endif
-				if (KNN_m[i][0].queryIdx >= keypoint_object.size())
-				{
-					continue;
-				}
-				lisx.push_back(((img_object.cols / 2.0 - keypoint_object[KNN_m[i][0].queryIdx].pt.x) * mapScale + keypoint_scene[KNN_m[i][0].trainIdx].pt.x));
-				lisy.push_back(((img_object.rows / 2.0 - keypoint_object[KNN_m[i][0].queryIdx].pt.y) * mapScale + keypoint_scene[KNN_m[i][0].trainIdx].pt.y));
-				sumx += lisx.back();
-				sumy += lisy.back();
-			}
-		}
-#ifdef _DEBUG
-		draw_good_matches(img_scene, keypoint_scene, img_object, keypoint_object, good_matches);
-#endif
-	}
+	void calc_good_matches(cv::Mat& img_scene, std::vector<cv::KeyPoint> keypoint_scene, cv::Mat& img_object, std::vector<cv::KeyPoint> keypoint_object, std::vector<std::vector<cv::DMatch>>& KNN_m, double ratio_thresh, double mapScale, std::vector<double>& lisx, std::vector<double>& lisy, double& sumx, double& sumy);
 
 
 }
