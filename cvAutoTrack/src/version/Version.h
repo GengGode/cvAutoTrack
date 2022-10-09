85b59d544472cd96128baae734ad3accee2cc17d
GengGode <47711102+GengGode@users.noreply.github.com>
2022-10-09 14:33:47 +0800

    Fix build version (#50)
    * chore.构建添加fix分支
    
    * feat.实现版本自动迭代
    
    * fix.修复远程编译未生成自动版本号的问题
    
    * fix.修复编译版本号的错误
    
    * fix.修复编译版本号比实际高1的问题
    
    编译版本号已经应用导致获取到的下一版本号实际为本次编译的下一版本号

--- cvAutoTrack/src/version/Version.h
+++ cvAutoTrack/src/version/Version.h
@@ -4,13 +4,13 @@ namespace TianLi::Version
    const int version_major = 7;
    const int version_minor = 0;
    const int version_revision = 1;
-   const int version_build = 182;
-   const std::string version_hash = "cde4b38";
-   const std::string build_version = "7.0.1.182-cde4b38";
+   const int version_build = 198;
+   const std::string version_hash = "d2b0caf";
+   const std::string build_version = "7.0.1.198-d2b0caf";
 #ifndef _DEBUG
-   const std::string build_time = "2022/10/09 ÖÜÈÕ";
+   const std::string build_time = "å‘¨æ—¥ 2022/10/09";
 #else
-   const std::string build_time = "2022/10/09 ÖÜÈÕ 2:56:51.28";
+   const std::string build_time = "å‘¨æ—¥ 2022/10/0914:04:41.13";
 #endif
 }
-//¸ÃÎÄ¼þ×Ô¶¯Éú³É£¬ÎÞÐè¸ü¸Ä 
+//ï¿½ï¿½ï¿½Ä¼ï¿½ï¿½Ô¶ï¿½ï¿½ï¿½ï¿½É£ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 
