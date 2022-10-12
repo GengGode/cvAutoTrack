0362d24be25729bde00d302ec71da0390392da4b
GengGode <47711102+GengGode@users.noreply.github.com>
2022-10-12 09:39:00 +0800

    Dev sem ver (#54)
    * feat.版本号中添加分支名称
    
    * feat.修订号每次编译加一
    
    * feat.版本号tag规则化，不再显示编译号
    
    * fix.修复分支名为空的问题
    
    * fix.修复release tag显示文本

--- cvAutoTrack/src/version/build_version_before.bat
+++ cvAutoTrack/src/version/build_version_before.bat
@@ -1,22 +1,39 @@
-del src\version\Version.h
-del src\version\version.ver
-del src\version\version_hash.hash
+echo build version.h before build
+
+if exist src\version\Version.h (
+	del src\version\Version.h
+)
+if exist src\version\version.ver (
+	del src\version\version.ver
+)
+if exist src\version\version.branch (
+	del src\version\version.branch
+)
+if exist src\version\version_hash.hash (
+	del src\version\version_hash.hash
+)
+if exist src\version\version_next.number (
+	del src\version\version_next.number
+)
 
 git describe --tags >> src\version\version.ver
+git branch --show-current >>  src\version\version.branch
 git log -n1 --format=format:"%%h" >> src\version\version_hash.hash
 powershell src\version\GetNextBuildVersion.ps1 >> src\version\version_next.number
 
 for /f %%x in (src\version\version.ver) do (
 	set version=%%x
 )
-for /f  "tokens=1,2,3,4,5,6 delims=.-" %%a  in  ("%version%")  do (
+for /f  "tokens=1,2,3 delims=.-" %%a  in  ("%version%")  do (
 	set v1=%%a
 	set v2=%%b
-	set v3=%%c
-	set v5=%%e
+	set /a v3=%%c+1
 )
 for /f %%x in (src\version\version_next.number) do (
 	set /a v4=%%x-1
+)
+for /f %%x in (src\version\version.branch) do (
+	set v5=%%x
 )
 for /f %%x in (src\version\version_hash.hash) do (
 	set v6=%%x
@@ -30,7 +47,7 @@ echo    const int version_minor = %v2%;>>src\version\Version.h
 echo    const int version_revision = %v3%;>>src\version\Version.h
 echo    const int version_build = %v4%;>>src\version\Version.h
 echo    const std::string version_hash = "%v6%";>>src\version\Version.h
-echo    const std::string build_version = "%v1%.%v2%.%v3%.%v4%-%v6%";>>src\version\Version.h
+echo    const std::string build_version = "%v1%.%v2%.%v3%.%v4%-%v5%-%v6%";>>src\version\Version.h
 echo %#ifndef _DEBUG>>src\version\Version.h
 echo    const std::string build_time = "%date%";>>src\version\Version.h
 echo #else>>src\version\Version.h
@@ -39,6 +56,9 @@ echo %#endif>>src\version\Version.h
 echo }>>src\version\Version.h
 echo //该文件自动生成，无需更改 >>src\version\Version.h
 
+echo build version : %v1%.%v2%.%v3%.%v4%-%v5%-%v6% 
+
 del src\version\version.ver
+del src\version\version.branch
 del src\version\version_hash.hash
 del src\version\version_next.number
