d2be549e095a3603a28d0c74d37f15417262fe8f
GengGode <GengGode@outlook.com>
2022-10-28 01:16:40 +0800

    fix.修复未初始化变量警告

--- cvAutoTrack/src/AutoTrack.cpp
+++ cvAutoTrack/src/AutoTrack.cpp
@@ -1314,14 +1314,14 @@ bool AutoTrack::getGengshinImpactScale()
 	HMONITOR hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
 
 	// 获取监视器逻辑宽度与高度
-	MONITORINFOEX miex;
+	MONITORINFOEX miex{};
 	miex.cbSize = sizeof(miex);
 	GetMonitorInfo(hMonitor, &miex);
 	int cxLogical = (miex.rcMonitor.right - miex.rcMonitor.left);
 	//int cyLogical = (miex.rcMonitor.bottom - miex.rcMonitor.top);
 
 	// 获取监视器物理宽度与高度
-	DEVMODE dm;
+	DEVMODE dm{};
 	dm.dmSize = sizeof(dm);
 	dm.dmDriverExtra = 0;
 	EnumDisplaySettings(miex.szDevice, ENUM_CURRENT_SETTINGS, &dm);
