22d04c307bb7da9c50d1f024ad82d8be928d8176
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2022-10-29 00:31:07 +0800

    Fix get direction (#66)
    * fix.修复了角色角度某个方向无法识别的问题
    
    * feat.调整阈值，降低了出错概率

--- cvAutoTrack/src/AutoTrack.cpp
+++ cvAutoTrack/src/AutoTrack.cpp
@@ -500,14 +500,15 @@ bool AutoTrack::GetDirection(double& a)
 	cv::resize(and12, and12, cv::Size(), 2, 2, 3);
 	cv::Canny(and12, and12, 20, 3 * 20, 3);
 	cv::circle(and12, cv::Point(cvCeil(and12.cols / 2), cvCeil(and12.rows / 2)), cvCeil(and12.cols / 4.5), cv::Scalar(0, 0, 0), -1);
-	cv::Mat dilate_element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
+	cv::Mat dilate_element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
 	cv::dilate(and12, and12, dilate_element);
 	cv::Mat erode_element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
 	cv::erode(and12, and12, erode_element);
 
 	std::vector<std::vector<cv::Point>> contours;
 	std::vector<cv::Vec4i> hierarcy;
-
+	cv::Point2d center = cv::Point2d(and12.cols / 2.0, and12.rows / 2.0);
+	
 	cv::findContours(and12, contours, hierarcy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 
 	std::vector<cv::Rect> boundRect(contours.size());  //定义外接矩形集合
@@ -518,43 +519,57 @@ bool AutoTrack::GetDirection(double& a)
 	std::vector<cv::Point2f> AvatarKeyLine;
 	cv::Point2f KeyLine;
 
-	if (contours.size() != 3)
+	if (contours.size() == 3)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			boundRect[i] = cv::boundingRect(cv::Mat(contours[i]));
+			AvatarKeyPoint.push_back(cv::Point2d(boundRect[i].x + boundRect[i].width / 2.0, boundRect[i].y + boundRect[i].height / 2.0));
+		}
+
+		AvatarKeyPointLine[0] = TianLi::Utils::dis(AvatarKeyPoint[2] - AvatarKeyPoint[1]);
+		AvatarKeyPointLine[1] = TianLi::Utils::dis(AvatarKeyPoint[2] - AvatarKeyPoint[0]);
+		AvatarKeyPointLine[2] = TianLi::Utils::dis(AvatarKeyPoint[1] - AvatarKeyPoint[0]);
+
+		if (AvatarKeyPointLine[0] >= AvatarKeyPointLine[2] && AvatarKeyPointLine[1] >= AvatarKeyPointLine[2])
+		{
+			AvatarKeyLine.push_back(AvatarKeyPoint[2] - AvatarKeyPoint[1]);
+			AvatarKeyLine.push_back(AvatarKeyPoint[2] - AvatarKeyPoint[0]);
+		}
+		if (AvatarKeyPointLine[0] >= AvatarKeyPointLine[1] && AvatarKeyPointLine[2] >= AvatarKeyPointLine[1])
+		{
+			AvatarKeyLine.push_back(AvatarKeyPoint[1] - AvatarKeyPoint[0]);
+			AvatarKeyLine.push_back(AvatarKeyPoint[1] - AvatarKeyPoint[2]);
+		}
+		if (AvatarKeyPointLine[1] >= AvatarKeyPointLine[0] && AvatarKeyPointLine[2] >= AvatarKeyPointLine[0])
+		{
+			AvatarKeyLine.push_back(AvatarKeyPoint[0] - AvatarKeyPoint[1]);
+			AvatarKeyLine.push_back(AvatarKeyPoint[0] - AvatarKeyPoint[2]);
+		}
+
+		AvatarKeyLine = TianLi::Utils::Vector2UnitVector(AvatarKeyLine);
+		KeyLine = AvatarKeyLine[0] + AvatarKeyLine[1];
+
+		a = TianLi::Utils::Line2Angle(KeyLine);
+	}
+	else if (contours.size() == 2)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			boundRect[i] = cv::boundingRect(cv::Mat(contours[i]));
+			AvatarKeyPoint.push_back(cv::Point2d(boundRect[i].x + boundRect[i].width / 2.0, boundRect[i].y + boundRect[i].height / 2.0));
+		}
+		// 两点取中点
+		AvatarKeyPoint.push_back((AvatarKeyPoint[0] + AvatarKeyPoint[1]) / 2.0);
+		// 中点基于图片中心对称位置
+		cv::Point2d line = center - AvatarKeyPoint[2];
+		a = TianLi::Utils::Line2Angle(line);
+	}
+	else
 	{
 		err = { 9,"提取小箭头特征误差过大" };
 		return false;
 	}
-
-	for (int i = 0; i < 3; i++)
-	{
-		boundRect[i] = cv::boundingRect(cv::Mat(contours[i]));
-		AvatarKeyPoint.push_back(cv::Point(cvRound(boundRect[i].x + boundRect[i].width / 2), cvRound(boundRect[i].y + boundRect[i].height / 2)));
-	}
-
-	AvatarKeyPointLine[0] = TianLi::Utils::dis(AvatarKeyPoint[2] - AvatarKeyPoint[1]);
-	AvatarKeyPointLine[1] = TianLi::Utils::dis(AvatarKeyPoint[2] - AvatarKeyPoint[0]);
-	AvatarKeyPointLine[2] = TianLi::Utils::dis(AvatarKeyPoint[1] - AvatarKeyPoint[0]);
-
-	if (AvatarKeyPointLine[0] >= AvatarKeyPointLine[2] && AvatarKeyPointLine[1] >= AvatarKeyPointLine[2])
-	{
-		AvatarKeyLine.push_back(AvatarKeyPoint[2] - AvatarKeyPoint[1]);
-		AvatarKeyLine.push_back(AvatarKeyPoint[2] - AvatarKeyPoint[0]);
-	}
-	if (AvatarKeyPointLine[0] >= AvatarKeyPointLine[1] && AvatarKeyPointLine[2] >= AvatarKeyPointLine[1])
-	{
-		AvatarKeyLine.push_back(AvatarKeyPoint[1] - AvatarKeyPoint[0]);
-		AvatarKeyLine.push_back(AvatarKeyPoint[1] - AvatarKeyPoint[2]);
-	}
-	if (AvatarKeyPointLine[1] >= AvatarKeyPointLine[0] && AvatarKeyPointLine[2] >= AvatarKeyPointLine[0])
-	{
-		AvatarKeyLine.push_back(AvatarKeyPoint[0] - AvatarKeyPoint[1]);
-		AvatarKeyLine.push_back(AvatarKeyPoint[0] - AvatarKeyPoint[2]);
-	}
-
-	AvatarKeyLine = TianLi::Utils::Vector2UnitVector(AvatarKeyLine);
-	KeyLine = AvatarKeyLine[0] + AvatarKeyLine[1];
-
-	a = TianLi::Utils::Line2Angle(KeyLine);
-	
 	return clear_error_logs();
 }
 
