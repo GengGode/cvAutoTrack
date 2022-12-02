f3e86ee6da720c7ab4f7fa8debdb1fa345031d36
GengGode <GengGode@outlook.com>
2022-12-02 12:20:49 +0800

    feat:日志添加系统信息

--- cvAutoTrack/src/ErrorCode.cpp
+++ cvAutoTrack/src/ErrorCode.cpp
@@ -1,9 +1,238 @@
 #include "pch.h"
 #include "ErrorCode.h"
 
+std::string get_sys_version()
+{
+	// Get the Windows version.
+	OSVERSIONINFOEX osvi;
+	BOOL bOsVersionInfoEx;
+
+	// Try calling GetVersionEx using the OSVERSIONINFOEX structure.
+	// If that fails, try using the OSVERSIONINFO structure.
+
+	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+
+	//if (!(bOsVersionInfoEx = GetVersionEx((OSVERSIONINFO*)&osvi)))
+	//{
+	//	// If OSVERSIONINFOEX doesn't work, try OSVERSIONINFO.
+
+	//	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	//	if (!GetVersionEx((OSVERSIONINFO*)&osvi))
+	//		return "Unknown";
+	//}
+
+	// get the major and minor version numbers
+	int major = osvi.dwMajorVersion;
+	int minor = osvi.dwMinorVersion;
+
+	// get the build number
+	int build = osvi.dwBuildNumber & 0xFFFF;
+
+	// get the service pack (if any) and build number
+	std::string service_pack = "";
+	//if (osvi.szCSDVersion[0] != '\0')
+	//{
+	//	service_pack = std::string(osvi.szCSDVersion);
+	//}
+
+	// get the product type
+	std::string product_type = "";
+	switch (osvi.wProductType)
+	{
+	case VER_NT_WORKSTATION:
+		if (major == 4)
+			product_type = "Workstation 4.0";
+		else if (major == 5 && minor == 0)
+			product_type = "Workstation 2000";
+		else if (major == 5 && minor == 1)
+			product_type = "Workstation XP";
+		else if (major == 6 && minor == 0)
+			product_type = "Workstation Vista";
+		else if (major == 6 && minor == 1)
+			product_type = "Workstation 7";
+		else if (major == 6 && minor == 2)
+			product_type = "Workstation 8";
+		else if (major == 6 && minor == 3)
+			product_type = "Workstation 8.1";
+		else if (major == 10 && minor == 0)
+			product_type = "Workstation 10";
+		else
+			product_type = "Workstation";
+		break;
+	case VER_NT_DOMAIN_CONTROLLER:
+		if (major == 4)
+			product_type = "Domain Controller 4.0";
+		else if (major == 5 && minor == 0)
+			product_type = "Domain Controller 2000";
+		else if (major == 5 && minor == 1)
+			product_type = "Domain Controller XP";
+		else if (major == 5 && minor == 2)
+			product_type = "Domain Controller 2003";
+		else if (major == 6 && minor == 0)
+			product_type = "Domain Controller Vista";
+		else if (major == 6 && minor == 1)
+			product_type = "Domain Controller 7";
+		else if (major == 6 && minor == 2)
+			product_type = "Domain Controller 8";
+		else if (major == 6 && minor == 3)
+			product_type = "Domain Controller 8.1";
+		else if (major == 10 && minor == 0)
+			product_type = "Domain Controller 10";
+		else
+			product_type = "Domain Controller";
+		break;
+	case VER_NT_SERVER:
+		if (major == 4)
+			product_type = "Server 4.0";
+		else if (major == 5 && minor == 0)
+			product_type = "Server 2000";
+		else if (major == 5 && minor == 1)
+			product_type = "Server XP";
+		else if (major == 5 && minor == 2)
+			product_type = "Server 2003";
+		else if (major == 6 && minor == 0)
+			product_type = "Server Vista";
+		else if (major == 6 && minor == 1)
+			product_type = "Server 7";
+		else if (major == 6 && minor == 2)
+			product_type = "Server 8";
+		else if (major == 6 && minor == 3)
+			product_type = "Server 8.1";
+		else if (major == 10 && minor == 0)
+			product_type = "Server 10";
+		else
+			product_type = "Server";
+		break;
+	default:
+		product_type = "Unknown";
+		break;
+	}
+
+	// get the architecture
+	std::string architecture = "";
+	SYSTEM_INFO si;
+	GetNativeSystemInfo(&si);	
+	if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
+		architecture = "x64";
+	else if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_INTEL)
+		architecture = "x86";
+	else
+		architecture = "Unknown";
+	
+	// return the version string
+	return product_type + " " + architecture + " " + std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(build) + " " + service_pack;
+	
+}
+std::string get_gpu_name()
+{
+	// Get the name of the GPU
+	IDXGIAdapter* pAdapter = nullptr;
+	IDXGIFactory* pFactory = nullptr;
+	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	hr = pFactory->EnumAdapters(0, &pAdapter);
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	DXGI_ADAPTER_DESC desc;
+	hr = pAdapter->GetDesc(&desc);
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	std::wstring ws(desc.Description);
+	std::string str(ws.begin(), ws.end());
+
+	return str;
+}
+std::string get_gpu_memory()
+{
+	// Get the amount of memory on the GPU
+	IDXGIAdapter* pAdapter = nullptr;
+	IDXGIFactory* pFactory = nullptr;
+	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	hr = pFactory->EnumAdapters(0, &pAdapter);
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	DXGI_ADAPTER_DESC desc;
+	hr = pAdapter->GetDesc(&desc);
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	return std::to_string(desc.DedicatedVideoMemory / 1024 / 1024) + " MB";
+}
+std::string get_directx_version()
+{
+	// 获取系统 DirectX 版本
+	DWORD dwVersion = 0;
+	DWORD dwMajorVersion = 0;
+	DWORD dwMinorVersion = 0;
+	DWORD dwBuild = 0;
+
+	//// Get the DirectX version.
+	//dwVersion = DirectXVersion();
+
+	//// Get the DirectX major version.
+	//dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
+
+	//// Get the DirectX minor version.
+
+	//dwMinorVersion = (DWORD)(HIBYTE(LOWORD(dwVersion)));
+
+	//// Get the build number.
+
+	//dwBuild = (DWORD)(HIWORD(dwVersion));
+
+	// return the version string
+	return std::to_string(dwMajorVersion) + "." + std::to_string(dwMinorVersion) + "." + std::to_string(dwBuild);
+}
+std::string get_sys_name()
+{
+	// 获取系统名称
+	std::string name = "";
+	char computerName[MAX_COMPUTERNAME_LENGTH + 1];
+	DWORD size = MAX_COMPUTERNAME_LENGTH + 1;
+	GetComputerNameA(computerName, &size);
+	name = computerName;
+	return name;
+}
+
 ErrorCode::ErrorCode()
 {
 	fopen_s(&fptr, "./autoTrack.log", "w+");
+	// 获取系统信息写入日志 系统名称 系统版本号 显卡 显存 DirectX版本
+	if (fptr == nullptr) return;
+	fprintf_s(fptr, "系统名称:");
+	fprintf_s(fptr, get_sys_name().c_str());
+	fprintf_s(fptr, "系统版本号:");
+	fprintf_s(fptr, get_sys_version().c_str());
+	fprintf_s(fptr, "显卡:");
+	fprintf_s(fptr, get_gpu_name().c_str());
+	fprintf_s(fptr, "显存:");
+	fprintf_s(fptr, get_gpu_memory().c_str());
+	fprintf_s(fptr, "DirectX版本:");
+	fprintf_s(fptr, get_directx_version().c_str());
+	fprintf_s(fptr, "\n");
+	
+	fflush(fptr);
 }
 
 ErrorCode::~ErrorCode()
