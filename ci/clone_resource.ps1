87bb7561f1f15973e8f875fba8c7b4ff1f93dbce
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-02-03 04:06:54 +0800

    update:ps1

---
+++ ci/clone_resource.ps1
@@ -0,0 +1,2 @@
+cd C:\projects\cvautotrack\
+git submodule update --recursive
--- ci/release_resource.ps1
+++ ci/release_resource.ps1
@@ -1,5 +1,4 @@
 $ResourcePkg='./cvAutoTrack.Resource/resource.part01.rar'
 $ResourceOutDir='./cvAutoTrack/'
 mkdir $ResourceOutDir
-git submodule update --recursive
 7z x $ResourcePkg -y -o"$ResourceOutDir"
