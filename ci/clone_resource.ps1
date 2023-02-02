d785d085e9585a7a54c11ea86e75f5280cad3169
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-02-03 04:38:57 +0800

    update:使用环境变量

--- ci/clone_resource.ps1
+++ ci/clone_resource.ps1
@@ -1,3 +1,2 @@
 cd C:\projects\cvautotrack\
-dir
 git submodule update --init --recursive
--- ci/release_resource.ps1
+++ ci/release_resource.ps1
@@ -1,4 +1,3 @@
 $ResourcePkg='./cvAutoTrack.Resource/resource.part01.rar'
 $ResourceOutDir='./cvAutoTrack/'
-mkdir $ResourceOutDir
 7z x $ResourcePkg -y -o"$ResourceOutDir"
