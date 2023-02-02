8d17ed764b3f8097d795aad7973c0fcf4d08c3d6
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-02-03 03:58:24 +0800

    update:used 7z

--- ci/release_resource.ps1
+++ ci/release_resource.ps1
@@ -2,4 +2,4 @@ $ResourcePkg='./cvAutoTrack.Resource/resource.part01.rar'
 $ResourceOutDir='./cvAutoTrack/'
 mkdir $ResourceOutDir
 git submodule update --recursive
-.\7z x $ResourcePkg -y -o"$ResourceOutDir"
+7z x $ResourcePkg -y -o"$ResourceOutDir"
