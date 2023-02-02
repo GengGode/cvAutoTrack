db1ec8a4ec9d297d55edb6c2af1e3820947d508c
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-02-03 03:54:11 +0800

    update: wget $OpenCvUrl -OutFile Opencv.zip

--- ci/install_opencv.ps1
+++ ci/install_opencv.ps1
@@ -1,6 +1,6 @@
 $OpenCvDir='C:\opencv\'
 $OpenCvUrl='https://github.com/GengGode/OpencvStaticLib/releases/download/4.6.0/Opencv.zip'
-mkdir $OpenCvDir
-wget $OpenCvUrl
+mkdir $OpenCvDir 
+wget $OpenCvUrl -OutFile Opencv.zip
 .\7z x ./*.zip -y -o"$OpenCvDir"
 rm ./*.zip
