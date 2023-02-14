08dc32a150818ceeefd4f4a7a4977583c322966a
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-02-14 22:56:35 +0800

    feat:日志中添加系统信息

--- cvAutoTrack/src/ErrorCode.cpp
+++ cvAutoTrack/src/ErrorCode.cpp
@@ -1,219 +1,59 @@
 #include "pch.h"
 #include "ErrorCode.h"
 
-//std::string get_sys_version()
-//{
-//	// Get the Windows version.
-//	OSVERSIONINFOEX osvi;
-//	BOOL bOsVersionInfoEx;
-//
-//	// Try calling GetVersionEx using the OSVERSIONINFOEX structure.
-//	// If that fails, try using the OSVERSIONINFO structure.
-//
-//	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
-//	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-//
-//	//if (!(bOsVersionInfoEx = GetVersionEx((OSVERSIONINFO*)&osvi)))
-//	//{
-//	//	// If OSVERSIONINFOEX doesn't work, try OSVERSIONINFO.
-//
-//	//	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-//	//	if (!GetVersionEx((OSVERSIONINFO*)&osvi))
-//	//		return "Unknown";
-//	//}
-//
-//	// get the major and minor version numbers
-//	int major = osvi.dwMajorVersion;
-//	int minor = osvi.dwMinorVersion;
-//
-//	// get the build number
-//	int build = osvi.dwBuildNumber & 0xFFFF;
-//
-//	// get the service pack (if any) and build number
-//	std::string service_pack = "";
-//	//if (osvi.szCSDVersion[0] != '\0')
-//	//{
-//	//	service_pack = std::string(osvi.szCSDVersion);
-//	//}
-//
-//	// get the product type
-//	std::string product_type = "";
-//	switch (osvi.wProductType)
-//	{
-//	case VER_NT_WORKSTATION:
-//		if (major == 4)
-//			product_type = "Workstation 4.0";
-//		else if (major == 5 && minor == 0)
-//			product_type = "Workstation 2000";
-//		else if (major == 5 && minor == 1)
-//			product_type = "Workstation XP";
-//		else if (major == 6 && minor == 0)
-//			product_type = "Workstation Vista";
-//		else if (major == 6 && minor == 1)
-//			product_type = "Workstation 7";
-//		else if (major == 6 && minor == 2)
-//			product_type = "Workstation 8";
-//		else if (major == 6 && minor == 3)
-//			product_type = "Workstation 8.1";
-//		else if (major == 10 && minor == 0)
-//			product_type = "Workstation 10";
-//		else
-//			product_type = "Workstation";
-//		break;
-//	case VER_NT_DOMAIN_CONTROLLER:
-//		if (major == 4)
-//			product_type = "Domain Controller 4.0";
-//		else if (major == 5 && minor == 0)
-//			product_type = "Domain Controller 2000";
-//		else if (major == 5 && minor == 1)
-//			product_type = "Domain Controller XP";
-//		else if (major == 5 && minor == 2)
-//			product_type = "Domain Controller 2003";
-//		else if (major == 6 && minor == 0)
-//			product_type = "Domain Controller Vista";
-//		else if (major == 6 && minor == 1)
-//			product_type = "Domain Controller 7";
-//		else if (major == 6 && minor == 2)
-//			product_type = "Domain Controller 8";
-//		else if (major == 6 && minor == 3)
-//			product_type = "Domain Controller 8.1";
-//		else if (major == 10 && minor == 0)
-//			product_type = "Domain Controller 10";
-//		else
-//			product_type = "Domain Controller";
-//		break;
-//	case VER_NT_SERVER:
-//		if (major == 4)
-//			product_type = "Server 4.0";
-//		else if (major == 5 && minor == 0)
-//			product_type = "Server 2000";
-//		else if (major == 5 && minor == 1)
-//			product_type = "Server XP";
-//		else if (major == 5 && minor == 2)
-//			product_type = "Server 2003";
-//		else if (major == 6 && minor == 0)
-//			product_type = "Server Vista";
-//		else if (major == 6 && minor == 1)
-//			product_type = "Server 7";
-//		else if (major == 6 && minor == 2)
-//			product_type = "Server 8";
-//		else if (major == 6 && minor == 3)
-//			product_type = "Server 8.1";
-//		else if (major == 10 && minor == 0)
-//			product_type = "Server 10";
-//		else
-//			product_type = "Server";
-//		break;
-//	default:
-//		product_type = "Unknown";
-//		break;
-//	}
-//
-//	// get the architecture
-//	std::string architecture = "";
-//	SYSTEM_INFO si;
-//	GetNativeSystemInfo(&si);	
-//	if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
-//		architecture = "x64";
-//	else if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_INTEL)
-//		architecture = "x86";
-//	else
-//		architecture = "Unknown";
-//	
-//	// return the version string
-//	return product_type + " " + architecture + " " + std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(build) + " " + service_pack;
-//	
-//}
-//std::string get_gpu_name()
-//{
-//	// Get the name of the GPU
-//	IDXGIAdapter* pAdapter = nullptr;
-//	IDXGIFactory* pFactory = nullptr;
-//	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
-//	if (FAILED(hr))
-//	{
-//		return "Unknown";
-//	}
-//
-//	hr = pFactory->EnumAdapters(0, &pAdapter);
-//	if (FAILED(hr))
-//	{
-//		return "Unknown";
-//	}
-//
-//	DXGI_ADAPTER_DESC desc;
-//	hr = pAdapter->GetDesc(&desc);
-//	if (FAILED(hr))
-//	{
-//		return "Unknown";
-//	}
-//
-//	std::wstring ws(desc.Description);
-//	std::string str(ws.begin(), ws.end());
-//
-//	return str;
-//}
-//std::string get_gpu_memory()
-//{
-//	// Get the amount of memory on the GPU
-//	IDXGIAdapter* pAdapter = nullptr;
-//	IDXGIFactory* pFactory = nullptr;
-//	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
-//	if (FAILED(hr))
-//	{
-//		return "Unknown";
-//	}
-//
-//	hr = pFactory->EnumAdapters(0, &pAdapter);
-//	if (FAILED(hr))
-//	{
-//		return "Unknown";
-//	}
-//
-//	DXGI_ADAPTER_DESC desc;
-//	hr = pAdapter->GetDesc(&desc);
-//	if (FAILED(hr))
-//	{
-//		return "Unknown";
-//	}
-//
-//	return std::to_string(desc.DedicatedVideoMemory / 1024 / 1024) + " MB";
-//}
-//std::string get_directx_version()
-//{
-//	// 获取系统 DirectX 版本
-//	DWORD dwVersion = 0;
-//	DWORD dwMajorVersion = 0;
-//	DWORD dwMinorVersion = 0;
-//	DWORD dwBuild = 0;
-//
-//	//// Get the DirectX version.
-//	//dwVersion = DirectXVersion();
-//
-//	//// Get the DirectX major version.
-//	//dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
-//
-//	//// Get the DirectX minor version.
-//
-//	//dwMinorVersion = (DWORD)(HIBYTE(LOWORD(dwVersion)));
-//
-//	//// Get the build number.
-//
-//	//dwBuild = (DWORD)(HIWORD(dwVersion));
-//
-//	// return the version string
-//	return std::to_string(dwMajorVersion) + "." + std::to_string(dwMinorVersion) + "." + std::to_string(dwBuild);
-//}
-//std::string get_sys_name()
-//{
-//	// 获取系统名称
-//	std::string name = "";
-//	char computerName[MAX_COMPUTERNAME_LENGTH + 1];
-//	DWORD size = MAX_COMPUTERNAME_LENGTH + 1;
-//	GetComputerNameA(computerName, &size);
-//	name = computerName;
-//	return name;
-//}
+std::string get_sys_version()
+{
+	// 运行一个cmd，获取版本号信息
+	const char* cmd = "cmd /c ver";
+	std::string result = "";
+	FILE* pipe = _popen(cmd, "r");
+	if (!pipe) return "获取系统版本管道打开失败";
+	try {
+		char buffer[128];
+		while (!feof(pipe)) {
+			if (fgets(buffer, 128, pipe) != NULL)
+				result += buffer;
+		}
+	}
+	catch (...) {
+		_pclose(pipe);
+		return "读取管道中系统版本出现错误";
+	}
+	_pclose(pipe);
+	// 删除前后换行符
+	result.erase(0, 1);
+	result.erase(result.length() - 1, 1);
+	return result;
+}
+std::string get_gpu_name()
+{
+	// Get the name of the GPU
+	IDXGIAdapter* pAdapter = nullptr;
+	IDXGIFactory* pFactory = nullptr;
+	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	hr = pFactory->EnumAdapters(0, &pAdapter);
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	DXGI_ADAPTER_DESC desc;
+	hr = pAdapter->GetDesc(&desc);
+	if (FAILED(hr))
+	{
+		return "Unknown";
+	}
+
+	std::wstring ws(desc.Description);
+	std::string str(ws.begin(), ws.end());
+
+	return str;
+}
 
 ErrorCode::ErrorCode()
 {
@@ -254,7 +94,7 @@ inline std::string time_stamp(const std::string& fmt = "%F %T")
 
 inline void write_log(std::fstream& log_file,const std::string& time_stamp, const int error_code, const std::string& error_message)
 {
-	int file_size = std::filesystem::file_size("./autoTrack.log");
+	auto file_size = std::filesystem::file_size("./autoTrack.log");
 	// 如果文件大于2MB，则清空文件，只保留末尾的1MB
 	if (file_size > 1024 * 1024 * 2)
 	{
@@ -265,12 +105,14 @@ inline void write_log(std::fstream& log_file,const std::string& time_stamp, cons
 		// 清空文件
 		log_file.close();
 		log_file.open("./autoTrack.log", std::ios::in | std::ios::out | std::ios::trunc);
+		log_file << "系统版本号：" << get_sys_version() << "\n";
+		log_file << "显卡：" << get_gpu_name() << "\n";
 		
 		// 保留末尾的512kb
-		int keep_size = 1024 * 1024;
-		int start = content.size() - keep_size;
-		int line_start = content.find_last_of("\n", start) + 1;
-		int end = content.size();
+		auto keep_size = 1024 * 1024;
+		auto start = content.size() - keep_size;
+		auto line_start = content.find_last_of("\n", start) + 1;
+		auto end = content.size();
 		std::string keep_content = content.substr(line_start, end);
 		log_file << keep_content;
 	}
@@ -296,6 +138,8 @@ ErrorCode& ErrorCode::operator=(const std::pair<int, string>& err_code_msg)
 		{
 			log_file.open("./autoTrack.log", std::ios::in | std::ios::out | std::ios::app);
 			if (log_file.is_open() == false) return *this;
+			log_file << "系统版本号：" << get_sys_version() << "\n";
+			log_file << "显卡："<<get_gpu_name() << "\n";
 		}
 		write_log(log_file, time_stamp(), code, msg);
 		log_file.flush();
@@ -320,35 +164,29 @@ bool ErrorCode::enableWirteFile()
 	is_use_file = true;
 	return true;
 }
+/*
+int ErrorCode::writeFile(char const* const format ,...)
+{
+	if (is_use_file == false) return -1;
+	if (fptr == nullptr)
+	{
+		fopen_s(&fptr, "./autoTrack.log", "w+");
+		// 获取系统信息写入日志 系统名称 系统版本号 显卡 显存 DirectX版本
+		//writeFile("系统版本号:");
+		//writeFile(get_sys_version().c_str());
+		//writeFile("显卡:");
+		//writeFile(get_gpu_name().c_str());
+		if (fptr == nullptr) return -2;
+	}
 
-//int ErrorCode::writeFile(char const* const format ,...)
-//{
-//	if (is_use_file == false) return -1;
-//	if (fptr == nullptr)
-//	{
-//		fopen_s(&fptr, "./autoTrack.log", "w+");
-//		// 获取系统信息写入日志 系统名称 系统版本号 显卡 显存 DirectX版本
-//		//writeFile("系统名称:");
-//		//writeFile(get_sys_name().c_str());
-//		//writeFile("系统版本号:");
-//		//writeFile(get_sys_version().c_str());
-//		//writeFile("显卡:");
-//		//writeFile(get_gpu_name().c_str());
-//		//writeFile("显存:");
-//		//writeFile(get_gpu_memory().c_str());
-//		//writeFile("DirectX版本:");
-//		//writeFile(get_directx_version().c_str());
-//		//writeFile("\n");
-//		if (fptr == nullptr) return -2;
-//	}
-//
-//	int _Result;
-//	va_list _ArgList;
-//	__crt_va_start(_ArgList, format);
-//	_Result = fprintf_s(fptr, format, _ArgList);
-//	__crt_va_end(_ArgList);
-//	return _Result;
-//}
+	int _Result;
+	va_list _ArgList;
+	__crt_va_start(_ArgList, format);
+	_Result = fprintf_s(fptr, format, _ArgList);
+	__crt_va_end(_ArgList);
+	return _Result;
+}
+*/
 
 ErrorCode::operator int()
 {
