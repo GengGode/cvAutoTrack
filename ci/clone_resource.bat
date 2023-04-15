85ac38b60f0bbdce6c12f1dc2978bfcb7f6d2b4b
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-04-16 04:28:12 +0800

    delete:删除无用ci脚本
     close #31

--- ci/clone_resource.bat
+++
@@ -1,6 +0,0 @@
-set OpenCvDir=C:\opencv\
-set OpenCvUrl=https://github.com/GengGode/OpencvStaticLib/releases/download/4.6.0/Opencv.zip
-mkdir %OpenCvDir%
-DownloadFile %OpenCvUrl%
-7z x ./*.zip -y -o%OpenCvDir%
-rm -f ./*.zip
