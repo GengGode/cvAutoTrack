f6bd05c696f316fef212edf3d3e8d596c954fd5e
Sallee1 <885331635@qq.com>
2023-06-19 17:47:17 +0800

    feat: 实现均值滤波器

--- cvAutoTrack/src/filter/smooth/Smooth.cpp
+++ cvAutoTrack/src/filter/smooth/Smooth.cpp
@@ -12,10 +12,12 @@ Smooth::~Smooth()
 
 cv::Point2d Smooth::filterting(const cv::Point2d& pos)
 {
-	return pos;
+	mean_pos =  pos * 0.1 + mean_pos * 0.9;
+	return mean_pos;
 }
 
 cv::Point2d Smooth::re_init_filterting(const cv::Point2d& pos)
 {
-	return pos;
+	mean_pos = pos;
+	return filterting(pos);
 }
