01b2ab1873d96bd307437c1ba905a596fc01141d
Sallee1 <885331635@qq.com>
2023-07-24 23:17:23 +0800

    feat:添加高精度的时间戳

--- cvAutoTrack/src/utils/Logger.h
+++ cvAutoTrack/src/utils/Logger.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <pch.h>
-#include <utils/Utils.h>
-
 //取消宏定义以防止潜在的命名冲突
 #pragma region UNDEF_LEVEL
 #undef ALL
@@ -15,6 +13,21 @@
 #pragma endregion UNDEF_LEVEL
 
 namespace TianLi::Utils {
+  inline std::string wstring2string(std::wstring wstr)
+  {
+    std::string result;
+    //获取缓冲区大小，并申请空间，缓冲区大小事按字节计算的  
+    int len = WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), (int)wstr.size(), NULL, 0, NULL, NULL);
+    char* buffer = new char[len + 1];
+    //宽字节编码转换成多字节编码  
+    WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), (int)wstr.size(), buffer, len, NULL, NULL);
+    buffer[len] = '\0';
+    //删除缓冲区并返回值  
+    result.append(buffer);
+    delete[] buffer;
+    return result;
+  }
+
   static constexpr int DEFAULT_EXPIRATION_TIME = 100;
   static constexpr LogType DEFAULT_LOG_LEVEL = LogType::NONE;
 
@@ -49,7 +62,8 @@ namespace TianLi::Utils {
     LogLevel level;             //日志级别
     LogType logType;            //日志类别
     std::string message;        //日志信息
-    std::time_t timestamp;      //时间戳
+    std::time_t timestamp;      //时间戳，用于输出日志时间
+    std::time_t microsecondStamp;   //微秒时间戳，用于区分日志的发起前后
     std::time_t expirationTime; //过期时间
 
     LogEntry() {};
@@ -57,6 +71,9 @@ namespace TianLi::Utils {
     LogEntry(LogLevel level, LogType logType, const std::string& message, std::time_t expirationTime)
       : level(level), logType(logType), message(message), expirationTime(expirationTime) {
       timestamp = std::time(nullptr);
+
+      auto currentTime = std::chrono::system_clock::now();
+      microsecondStamp = std::chrono::duration_cast<std::chrono::microseconds>(currentTime.time_since_epoch()).count();
     }
 
     bool isExpired() const {
@@ -68,7 +85,7 @@ namespace TianLi::Utils {
   private:
     bool consoleOutput;     //是否开启控制台输出
     LogLevel logLevel;      //日志级别
-    std::fstream logFile;     //日志的文件流
+    std::fstream logFile;   //日志的文件流
     size_t maxSize;
 
     /// <summary>
@@ -78,8 +95,8 @@ namespace TianLi::Utils {
     /// 使用散列值而不是日志本身作为键，是为了适配使用LogType单独标记类型的情况
     /// 
     /// logQueue用于按照时间戳排序日志，用于积极的过期日志清理
-    /// 目前需要重新设计数据结构，考虑利用chrono::high_resolution_clock添加微秒时间戳，
-    /// 这样在消极的过期日志清理时能自动调整结构
+    /// 使用微秒时间戳精确记录日志前后关系
+    /// 在消极的过期日志清理时能自动调整结构
     /// 当logQueue过长会执行积极的日志清理
     /// 积极的清理最好还是不要发生，这意味着日志的分类不合理，比如没有把含有变量的日志单独分类
     /// 如果发生积极的清理行为，则会额外打印WARN日志提醒
@@ -205,7 +222,7 @@ namespace TianLi::Utils {
     {
       if (logFile.tellp() >= maxSize)     //日志过大，裁剪其中一半内容
       {
-        auto buf = make_unique<char[]>(maxSize);
+        auto buf = std::make_unique<char[]>(maxSize);
         logFile.seekp(maxSize / 2);
         logFile.read(buf.get(), maxSize);
         logFile.seekp(0);
@@ -274,7 +291,5 @@ namespace TianLi::Utils {
         return "获取GPU信息失败";
       }
     }
-
-
   };
 };
