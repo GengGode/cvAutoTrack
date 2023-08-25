a079af3c23447714e1c8771d0343709afba2cfc7
Sallee1 <885331635@qq.com>
2023-08-25 20:56:57 +0800

    水个提交，不然ci动不了

--- cvAutoTrack/src/capture/dxgi/Dxgi.cpp
+++ cvAutoTrack/src/capture/dxgi/Dxgi.cpp
@@ -323,7 +323,7 @@ bool Dxgi::capture(cv::Mat& frame)
         frame = cv::Mat(frame_size.Height, frame_size.Width, CV_8UC4, (void*)data, pitch);
     }
     catch(std::exception e){
-        err = { 504, std::format("FATAL!! 出现了致命的错误，已自动跳过，原因:{}",e.what()) };
+        err = { 504, std::format("FATAL!! 出现了致命的错误，已自动跳过，原因:{}",e.what())};
         return false;
     }
     if (frame_size.Width < 480 || frame_size.Height < 360)
