3e5fe0b60c0dc2115e03f4e3d6851e85409f933f
Sallee1 <885331635@qq.com>
2023-09-11 02:55:53 +0800

    feat:完善json读写中

--- cvAutoTrack/src/resources/AutoTrackCacheGenerator.cpp
+++ cvAutoTrack/src/resources/AutoTrackCacheGenerator.cpp
@@ -25,7 +25,7 @@ namespace Tianli::Resource {
         }
 
         //打开json文件，并存入到字符串中
-        std::string_view jsonString;
+        std::string jsonString;
 
         std::fstream jsonFile;
         jsonFile.open(jsonPath, std::ios::in);
@@ -38,6 +38,103 @@ namespace Tianli::Resource {
 
         //将json字符串转换为json对象
         auto jsonDoc = json::parse5(jsonString);
+        json::object rootObj = jsonDoc.value().as_object();
+
+        //解析设置串
+        loadSettingFromJson(rootObj);
+        //解析自动拼图表串
+        loadTiledConfFromJson(rootObj);
+        //解析手动拼贴串
+        return true;
+    }
+
+    bool AutoTrackCacheGenerator::loadSettingFromJson(json::object root)
+    {
+        json::object settingObj = root["setting"].as_object();
+        m_setting_conf.version = settingObj["version"].as_string();
+        //读取SURF参数
+        m_setting_conf.hessian_threshold = settingObj["hessian_threshold"].as_double();
+        m_setting_conf.octave = settingObj["octave"].as_integer();
+        m_setting_conf.extended = settingObj["extended"].as_boolean();
+        m_setting_conf.upRight = settingObj["upRight"].as_boolean();
+        //读取其他设置参数
+        std::string position_mode = settingObj["position_mode"].as_string();
+        if (position_mode == "rect")
+            m_setting_conf.position_mode = E_RectType::RECT;
+        else if (position_mode == "range")
+            m_setting_conf.position_mode = E_RectType::RANGE;
+
+        m_setting_conf.resource_root_path = settingObj["resource_root_path"].as_string();
+        m_setting_conf.auto_tile_path_regex = settingObj["auto_tile_path_regex"].as_string();
+
+        return true;
+    }
+
+    bool AutoTrackCacheGenerator::loadTiledConfFromJson(json::object root)
+    {
+        json::array tiledArray = root["tiled"].as_array();
+
+        for (auto& it : tiledArray)
+        {
+            json::object tiledObj = it.as_object();
+            S_TiledConf tiledConf;
+            //获取区域id
+            tiledConf.area_id = tiledObj["area_id"].as_integer();
+            //获取区域文件名标识
+            tiledConf.area_filename = tiledObj["area_filename"].as_string();
+            //获取区域名称
+            tiledConf.name = tiledObj["name"].as_string();
+
+            //获取映射输入坐标系
+            auto zero_tile_rect_in = tiledObj["zero_tile_rect_in"].as_array();
+            tiledConf.zero_tile_rect_in.x = zero_tile_rect_in[0].as_integer();
+            tiledConf.zero_tile_rect_in.y = zero_tile_rect_in[1].as_integer();
+
+            if (m_setting_conf.position_mode == E_RectType::RECT)
+            {
+                tiledConf.zero_tile_rect_in.width = zero_tile_rect_in[2].as_integer();
+                tiledConf.zero_tile_rect_in.height = zero_tile_rect_in[3].as_integer();
+            }
+            else if (m_setting_conf.position_mode == E_RectType::RANGE)
+            {
+                tiledConf.zero_tile_rect_in.width = zero_tile_rect_in[2].as_integer() - zero_tile_rect_in[0].as_integer();
+                tiledConf.zero_tile_rect_in.height = zero_tile_rect_in[3].as_integer() - zero_tile_rect_in[1].as_integer();
+            }
+            //获取映射输出坐标系
+            auto zero_tile_rect_out = tiledObj["zero_tile_rect_out"].as_array();
+            tiledConf.zero_tile_rect_out.x = zero_tile_rect_out[0].as_integer();
+            tiledConf.zero_tile_rect_out.y = zero_tile_rect_out[1].as_integer();
+            if (m_setting_conf.position_mode == E_RectType::RECT)
+            {
+                tiledConf.zero_tile_rect_out.width = zero_tile_rect_out[2].as_integer();
+                tiledConf.zero_tile_rect_out.height = zero_tile_rect_out[3].as_integer();
+            }
+            else if (m_setting_conf.position_mode == E_RectType::RANGE)
+            {
+                tiledConf.zero_tile_rect_out.width = zero_tile_rect_out[2].as_integer() - zero_tile_rect_out[0].as_integer();
+                tiledConf.zero_tile_rect_out.height = zero_tile_rect_out[3].as_integer() - zero_tile_rect_out[1].as_integer();
+            }
+
+            //获取x，y范围
+            auto x_range = tiledObj["x_range"].as_array();
+            tiledConf.x_range.start = x_range[0].as_integer();
+            tiledConf.x_range.end = x_range[1].as_integer();
+
+            auto y_range = tiledObj["y_range"].as_array();
+            tiledConf.y_range.start = y_range[0].as_integer();
+            tiledConf.y_range.end = y_range[1].as_integer();
+
+            //获取tile尺寸
+            auto tile_size = tiledObj["tile_size"].as_array();
+            tiledConf.tile_size.width = tile_size[0].as_integer();
+            tiledConf.tile_size.height = tile_size[1].as_integer();
+
+            //获取缩放
+            tiledConf.zoom = tiledObj["zoom"].as_double();
+
+            //将结果压入数组
+            m_tiled_conf.emplace_back(tiledConf);
+        }
         return true;
     }
 }
--- cvAutoTrack/src/resources/AutoTrackCacheGenerator.h
+++ cvAutoTrack/src/resources/AutoTrackCacheGenerator.h
@@ -1,10 +1,61 @@
 #pragma once
+#include <pch.h>
+#include <json5.hpp>
+
 namespace Tianli::Resource
 {
     class AutoTrackCacheGenerator {
     private:
         AutoTrackCacheGenerator() = default;
         static AutoTrackCacheGenerator* instance;
+
+        enum E_RectType {
+            RECT, RANGE
+        };
+        //基本设置结构体
+        struct S_SettingConf {
+            //版本设置，与游戏版本保持一致
+            std::string version;
+            //以下为SURF::create的参数
+            double hessian_threshold;
+            int octave;
+            int octave_layers;
+            bool extended;
+            bool upRight;
+            //json中Rect的表示格式
+            E_RectType position_mode;
+            //资源的根目录的位置
+            std::string resource_root_path;
+            //使用自动拼图表的匹配表达式
+            std::string auto_tile_path_regex;
+        };
+        //自动接图表结构体
+        struct S_TiledConf {
+            int area_id;
+            std::string area_filename;
+            std::string name;
+            cv::Rect2i zero_tile_rect_in;
+            cv::Rect2i zero_tile_rect_out;
+            cv::Range x_range;
+            cv::Range y_range;
+            cv::Size2i tile_size;
+            float zoom;
+        };
+
+        //手动拼贴结构体
+        struct S_AreaConf {
+            int area_id;
+            std::string name;
+            std::string path;
+            cv::Rect2i area_rect;
+            cv::Size2i tile_size;
+            float zoom;
+        };
+
+        S_SettingConf m_setting_conf;
+        std::vector<S_TiledConf> m_tiled_conf;
+
+        std::map<int, std::vector<S_AreaConf>> m_areas_conf;
 
     public:
         static AutoTrackCacheGenerator* getInstance();
@@ -14,5 +65,9 @@ namespace Tianli::Resource
         // @param resourcePath 资源路径
         // @return 缓存是否生成成功
         bool loadJson(std::string const& jsonPath = "./AutoTrack_config.json5");
+
+    private:
+        bool loadSettingFromJson(json::object root);
+        bool loadTiledConfFromJson(json::object root);
     };
 }
