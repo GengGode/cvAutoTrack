549d9c480f9620aca1e6558832e86c92725abe9f
Sallee1 <885331635@qq.com>
2023-11-06 02:07:42 +0800

    refactor:清理Resource里被注释的代码

--- cvAutoTrack/src/resources/Resources.cpp
+++ cvAutoTrack/src/resources/Resources.cpp
@@ -4,7 +4,8 @@
 #include "resources/import/resources.import.h"
 #include <wincodec.h>
 
-#include "serialize.h"
+#include "resources/trackCache.h"
+
 #include "version/Version.h"
 
 namespace TianLi::Resource::Utils
@@ -35,140 +36,7 @@ namespace TianLi::Resource::Utils
         _mat = v_mat;
         return true;
     }
-    //bool
-    //	imdecode_(const cv::Mat& buf, int flags, cv::Mat& mat)
-    //{
-    //	CV_Assert(!buf.empty());
-    //	CV_Assert(buf.isContinuous());
-    //	CV_Assert(buf.checkVector(1, CV_8U) > 0);
-    //	cv::Mat buf_row = buf.reshape(1, 1);  // decoders expects single row, avoid issues with vector columns
-
-    //	cv::String filename;
-
-    //	cv::ImageDecoder decoder = findDecoder(buf_row);
-    //	if (!decoder)
-    //		return 0;
-
-    //	int scale_denom = 1;
-    //	if (flags > IMREAD_LOAD_GDAL)
-    //	{
-    //		if (flags & IMREAD_REDUCED_GRAYSCALE_2)
-    //			scale_denom = 2;
-    //		else if (flags & IMREAD_REDUCED_GRAYSCALE_4)
-    //			scale_denom = 4;
-    //		else if (flags & IMREAD_REDUCED_GRAYSCALE_8)
-    //			scale_denom = 8;
-    //	}
-
-    //	/// set the scale_denom in the driver
-    //	decoder->setScale(scale_denom);
-
-    //	if (!decoder->setSource(buf_row))
-    //	{
-    //		filename = tempfile();
-    //		FILE* f = fopen(filename.c_str(), "wb");
-    //		if (!f)
-    //			return 0;
-    //		size_t bufSize = buf_row.total() * buf.elemSize();
-    //		if (fwrite(buf_row.ptr(), 1, bufSize, f) != bufSize)
-    //		{
-    //			fclose(f);
-    //			CV_Error(Error::StsError, "failed to write image data to temporary file");
-    //		}
-    //		if (fclose(f) != 0)
-    //		{
-    //			CV_Error(Error::StsError, "failed to write image data to temporary file");
-    //		}
-    //		decoder->setSource(filename);
-    //	}
-
-    //	bool success = false;
-    //	try
-    //	{
-    //		if (decoder->readHeader())
-    //			success = true;
-    //	}
-    //	catch (const cv::Exception& e)
-    //	{
-    //		std::cerr << "imdecode_('" << filename << "'): can't read header: " << e.what() << std::endl << std::flush;
-    //	}
-    //	catch (...)
-    //	{
-    //		std::cerr << "imdecode_('" << filename << "'): can't read header: unknown exception" << std::endl << std::flush;
-    //	}
-    //	if (!success)
-    //	{
-    //		decoder.release();
-    //		if (!filename.empty())
-    //		{
-    //			if (0 != remove(filename.c_str()))
-    //			{
-    //				std::cerr << "unable to remove temporary file:" << filename << std::endl << std::flush;
-    //			}
-    //		}
-    //		return 0;
-    //	}
-
-    //	// established the required input image size
-    //	Size size = validateInputImageSize(Size(decoder->width(), decoder->height()));
-
-    //	int type = decoder->type();
-    //	if ((flags & IMREAD_LOAD_GDAL) != IMREAD_LOAD_GDAL && flags != IMREAD_UNCHANGED)
-    //	{
-    //		if ((flags & IMREAD_ANYDEPTH) == 0)
-    //			type = CV_MAKETYPE(CV_8U, CV_MAT_CN(type));
-
-    //		if ((flags & IMREAD_COLOR) != 0 ||
-    //			((flags & IMREAD_ANYCOLOR) != 0 && CV_MAT_CN(type) > 1))
-    //			type = CV_MAKETYPE(CV_MAT_DEPTH(type), 3);
-    //		else
-    //			type = CV_MAKETYPE(CV_MAT_DEPTH(type), 1);
-    //	}
-
-    //	mat.create(size.height, size.width, type);
-
-    //	success = false;
-    //	try
-    //	{
-    //		if (decoder->readData(mat))
-    //			success = true;
-    //	}
-    //	catch (const cv::Exception& e)
-    //	{
-    //		std::cerr << "imdecode_('" << filename << "'): can't read data: " << e.what() << std::endl << std::flush;
-    //	}
-    //	catch (...)
-    //	{
-    //		std::cerr << "imdecode_('" << filename << "'): can't read data: unknown exception" << std::endl << std::flush;
-    //	}
-
-    //	if (!filename.empty())
-    //	{
-    //		if (0 != remove(filename.c_str()))
-    //		{
-    //			std::cerr << "unable to remove temporary file:" << filename << std::endl << std::flush;
-    //		}
-    //	}
-
-    //	if (!success)
-    //	{
-    //		mat.release();
-    //		return false;
-    //	}
-
-    //	if (decoder->setScale(scale_denom) > 1) // if decoder is JpegDecoder then decoder->setScale always returns 1
-    //	{
-    //		resize(mat, mat, Size(size.width / scale_denom, size.height / scale_denom), 0, 0, INTER_LINEAR_EXACT);
-    //	}
-
-    //	/// optionally rotate the data if EXIF' orientation flag says so
-    //	if (!mat.empty() && (flags & IMREAD_IGNORE_ORIENTATION) == 0 && flags != IMREAD_UNCHANGED)
-    //	{
-    //		ApplyExifOrientation(decoder->getExifTag(ORIENTATION), mat);
-    //	}
-
-    //	return true;
-    //}
+ 
     void LoadImg_ID2Mat(int IDB, cv::Mat& mat, const wchar_t* format = L"PNG", int depth = 4)
     {
         IWICStream* pIWICStream = NULL;
@@ -193,19 +61,7 @@ namespace TianLi::Resource::Utils
 
         pImageFile = LockResource(imageResDataHandle);
         imageFileSize = SizeofResource(hModu, imageResHandle);
-        /*
-        {
-          // 2nd
-          char * ptr_data = (char*)malloc(imageFileSize);
-          memcpy(ptr_data, pImageFile, imageFileSize);
-          // to bin file
-          cv::Mat mat_array = cv::Mat(1, (int)imageFileSize, CV_8UC1, (void*)ptr_data);
-          // Debug下 imdecode 中 validateInputImageSize 中的 CV_IO_MAX_IMAGE_WIDTH 为0导致CV_Assert失败
-          cv::Mat v_mat = cv::imdecode(mat_array, cv::IMREAD_UNCHANGED);
-          mat = v_mat;
-          free(ptr_data);
-        }
-        */
+
 
         CoInitializeEx(NULL, COINIT_MULTITHREADED);
 
@@ -297,8 +153,6 @@ Resources::Resources()
     {
         cv::cvtColor(UIDnumber[i], UIDnumber[i], cv::COLOR_RGBA2GRAY);
     }
-
-    // install();
 }
 
 Resources::~Resources()
@@ -351,78 +205,14 @@ bool Resources::map_is_embedded()
     return true;
 }
 
-inline void MapKeypointCache::serialize(std::string outFileName)
-{
-    std::ofstream ofs(outFileName, std::fstream::out | std::fstream::binary);
-    Tianli::Resources::Utils::serializeStream ss(ofs);
-    ss << this->bulid_time;
-    ss << this->bulid_version;
-    ss << this->hessian_threshold;
-    ss << this->octave;
-    ss << this->octave_layers;
-    ss << this->extended;
-    ss << this->upRight;
-    ss << this->keyPoints;
-    ss << this->descriptors;
-    ss << this->bulid_version_end;
-    ss.align();
-    ofs.close();
-}
-
-inline void MapKeypointCache::deSerialize(std::string infileName)
-{
-    std::ifstream ifs(infileName, std::fstream::out | std::fstream::binary);
-    Tianli::Resources::Utils::deSerializeStream dss(ifs);
-    dss >> this->bulid_time;
-    dss >> this->bulid_version;
-    dss >> this->hessian_threshold;
-    dss >> this->octave;
-    dss >> this->octave_layers;
-    dss >> this->extended;
-    dss >> this->upRight;
-    dss >> this->keyPoints;
-    dss >> this->descriptors;
-    dss >> this->bulid_version_end;
-    ifs.close();
-}
-
-
-bool save_map_keypoint_cache(std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors, double hessian_threshold, int octaves, int octave_layers, bool extended, bool upright)
-{
-    cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create(hessian_threshold, octaves, octave_layers, extended, upright);
-    detector->detectAndCompute(Resources::getInstance().MapTemplate, cv::noArray(), keypoints, descriptors);
-
-    std::string build_time = __DATE__ " " __TIME__;
-
-    MapKeypointCache cache(
-        build_time, TianLi::Version::build_version, hessian_threshold,
-        (WORD)octaves, (WORD)octave_layers, (WORD)extended, (WORD)upright,
-        keypoints, descriptors);
-
-    cache.serialize("cvAutoTrack_Cache.xml");
-
-    return true;
-}
 bool load_map_keypoint_cache(std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors)
 {
-    if (std::filesystem::exists("cvAutoTrack_Cache.xml") == false)
+    if (std::filesystem::exists("cvAutoTrack_Cache.dat") == false)
     {
         return false;
     }
 
-    MapKeypointCache cache;
-    try {
-        cache.deSerialize("cvAutoTrack_Cache.xml");
-    }
-    catch (std::exception) {   //缓存损坏
-        return false;
-    }
 
-    if (cache.bulid_version != TianLi::Version::build_version)    //版本不一致
-        return false;
-
-    if (cache.bulid_version != cache.bulid_version_end)    //写入不完整
-        return false;
 
     keypoints = cache.keyPoints;
     descriptors = cache.descriptors;
@@ -431,12 +221,6 @@ bool load_map_keypoint_cache(std::vector<cv::KeyPoint>& keypoints, cv::Mat& desc
 
 bool get_map_keypoint(std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors)
 {
-    if (load_map_keypoint_cache(keypoints, descriptors) == false)
-    {
-        return save_map_keypoint_cache(keypoints, descriptors);
-    }
-    else
-    {
-        return true;
-    }
+    return load_map_keypoint_cache(keypoints, descriptors);
+
 }
