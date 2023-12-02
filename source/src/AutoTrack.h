e6787ca89efd820ec66b4aebbd317abb7949aa30
Sallee <885331635@qq.com>
2023-12-02 16:18:38 +0800

    refactor: 将cvAutoTrack使用单例模式构造 原本的全局变量_at可能会在其他全局变量之前构造，并且_at使用了其他全局变量，导致异常

--- source/src/AutoTrack.h
+++ source/src/AutoTrack.h
@@ -5,11 +5,16 @@ class Resources;
 class ErrorCode;
 class AutoTrack
 {
-    Resources &res;
-    ErrorCode &err;
+    Resources& res;
+    ErrorCode& err;
 
-public:
+private:
     AutoTrack();
+public:
+    static AutoTrack& Instance() {
+        static AutoTrack _instance;
+        return _instance;
+    }
 
 #pragma region 外部接口
     bool init();
@@ -18,31 +23,31 @@ public:
     bool SetUseBitbltCaptureMode();
     bool SetUseDx11CaptureMode();
 
-    bool ImportMapBlock(int id_x, int id_y, const char *image_data, int image_data_size, int image_width, int image_height);
+    bool ImportMapBlock(int id_x, int id_y, const char* image_data, int image_data_size, int image_width, int image_height);
     bool ImportMapBlockCenter(int x, int y);
     bool ImportMapBlockCenterScale(int x, int y, double scale);
 
     bool SetHandle(long long int handle = 0);
     bool SetWorldCenter(double x, double y);
     bool SetWorldScale(double scale);
-    bool GetTransformOfMap(double &x, double &y, double &a, int &mapId);
-    bool GetPosition(double &x, double &y);
-    bool GetPositionOfMap(double &x, double &y, int &mapId);
-    bool GetDirection(double &a);
-    bool GetRotation(double &a);
+    bool GetTransformOfMap(double& x, double& y, double& a, int& mapId);
+    bool GetPosition(double& x, double& y);
+    bool GetPositionOfMap(double& x, double& y, int& mapId);
+    bool GetDirection(double& a);
+    bool GetRotation(double& a);
     // 获取发现的神瞳坐标,isEnd为真则为当前画面中的最后一个神瞳
-    bool GetStar(double &x, double &y, bool &isEnd);
+    bool GetStar(double& x, double& y, bool& isEnd);
     // 获取发现的神瞳坐标，以json字符串格式
-    bool GetStarJson(char *jsonBuff);
-    bool GetUID(int &uid);
-    bool GetAllInfo(double &x, double &y, int &mapId, double &a, double &r, int &uid);
+    bool GetStarJson(char* jsonBuff);
+    bool GetUID(int& uid);
+    bool GetAllInfo(double& x, double& y, int& mapId, double& a, double& r, int& uid);
     /*********/
-    bool GetInfoLoadPicture(char *path, int &uid, double &x, double &y, double &a);
-    bool GetInfoLoadVideo(char *path, char *pathOutFile);
+    bool GetInfoLoadPicture(char* path, int& uid, double& x, double& y, double& a);
+    bool GetInfoLoadVideo(char* path, char* pathOutFile);
     /*********/
     int GetLastError();
-    int GetLastErrMsg(char *msg_buff, int buff_size);
-    int GetLastErrJson(char *json_buff, int buff_size);
+    int GetLastErrMsg(char* msg_buff, int buff_size);
+    int GetLastErrJson(char* json_buff, int buff_size);
 
     bool startServe();
     bool stopServe();
@@ -50,12 +55,12 @@ public:
     bool SetDisableFileLog();
     bool SetEnableFileLog();
 
-    bool GetVersion(char *version_buff, int buff_size);
-    bool GetCompileTime(char *time_buff, int buff_size);
+    bool GetVersion(char* version_buff, int buff_size);
+    bool GetCompileTime(char* time_buff, int buff_size);
     bool GetMapIsEmbedded();
 
     bool DebugCapture();
-    bool DebugCapturePath(const char *path_buff, int buff_size);
+    bool DebugCapturePath(const char* path_buff, int buff_size);
 #pragma endregion
 private:
     GenshinHandle genshin_handle;
