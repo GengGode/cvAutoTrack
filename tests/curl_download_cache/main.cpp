1acaae485bcca67a167194982a3836140153d2f4
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-12-06 07:05:56 +0000

    changed:调整缓存下载路径

--- tests/curl_download_cache/main.cpp
+++ tests/curl_download_cache/main.cpp
@@ -3,26 +3,11 @@
 #include <filesystem>
 #include <fstream>
 
-std::string cache_name_url = "https://gitee.com/WeiXiTianLi/cv-auto-track.cache-storge/raw/v20231205/cache_name";
-std::string cache_url = "https://gitee.com/WeiXiTianLi/cv-auto-track.cache-storge/raw/v20231205/";
+std::string cache_url = "https://gitee.com/WeiXiTianLi/cv-auto-track.cache-storge/releases/download/v20231205/cvAutoTrack_Cache.dat";
 
 int main()
 {
-    std::string get_cache_name_cmd = "curl -s " + cache_name_url + " -o cache_name";
-    auto get_cache_name_res = std::system(get_cache_name_cmd.c_str());
-    if(get_cache_name_res != 0)
-    {
-        std::cout << "get cache name failed" << std::endl;
-        return 0;
-    }
-
-    std::string cache_name;
-    std::ifstream cache_name_file("cache_name", std::ios::in);
-    cache_name_file >> cache_name;
-    cache_name_file.close();
-    std::cout << "cache name:" << cache_name << std::endl;
-
-    std::string get_cache_cmd = std::string("curl -s ") + cache_url + cache_name + " -o cvAutoTrack_Cache.dat" ;
+    std::string get_cache_cmd = std::string("curl -LJO ") + cache_url + " -o cvAutoTrack_Cache.dat" ;
     auto get_cache_res = std::system(get_cache_cmd.c_str());
     if(get_cache_res != 0)
     {
@@ -37,7 +22,4 @@ int main()
 
     std::cout << "cmd:" << get_cache_cmd << std::endl;
     std::cout << "get cache success" << std::endl;
-
-
-
 }
