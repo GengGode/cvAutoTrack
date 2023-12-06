3606bd3b04fe71605fc8ab591effbc697deff124
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-12-06 07:45:10 +0000

    changed:改用内置的tree并测试了内存开销

--- tests/container_treeset/main.cpp
+++ tests/container_treeset/main.cpp
@@ -2,26 +2,51 @@
 #include <Windows.h>
 #include <iostream>
 #include <opencv2/xfeatures2d/nonfree.hpp>
-#include "resources/binary/resources.binary.h"
 #include <fmt/format.h>
 #include "algorithms/algorithms.features.h"
+#include "algorithms/container/container.set.h"
 #include "resources/Resources.h"
-#include "tianli.realsky/tianli.core/core.map/item.set.h"
-using namespace tianli::algorithms::features;
 
-double test_time(int max_width, int max_height, std::vector<std::shared_ptr<ItemInface>> key_point_objects)
+using namespace tianli::algorithms::features;
+#include <psapi.h>
+
+
+double test_time(int max_width, int max_height, std::vector<std::shared_ptr<point_index>> key_point_objects)
 {
+    int test_cycle = 100;
+    int test_count = 0;
+
     auto start = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < 100; i++)
+    int mem_used_before = 0;
+    int mem_used_after = 0;
+
+    PROCESS_MEMORY_COUNTERS pmc;
+    auto mem_info = GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+    if (mem_info)
     {
-        auto item_set = MapSet(cv::Rect2d(0, 0, max_width, max_height), key_point_objects);
+        mem_used_before = pmc.WorkingSetSize;
+    }
+
+    for (int i = 0; i < test_cycle; i++)
+    {
+        test_count++;
+        auto item_set = tianli::algorithms::container::tree(key_point_objects);
+
+        mem_info = GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+        if (mem_info)
+        {
+            mem_used_after = pmc.WorkingSetSize;
+        }
+
+        double mem_used = (mem_used_after - mem_used_before) / 1024.0 / 1024.0;
+        std::cout << "test_count:" << test_count << " mem_used:" << mem_used << std::endl;
     }
 
     auto end = std::chrono::high_resolution_clock::now();
     return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1000000.0 / 100.0;
 }
-int main()
+int main(int v, char *s[])
 {
     features fts;
     std::shared_ptr<trackCache::CacheInfo> cache_info;
@@ -44,15 +69,15 @@ int main()
     drawKeypoints(mat, fts.keypoints, mat, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
     cv::imwrite(fmt::format("out_key_.png"), mat);
 
-    std::vector<std::shared_ptr<ItemInface>> key_point_objects;
+    std::vector<std::shared_ptr<point_index>> key_point_objects;
     for (auto &kp : cache_info->key_points)
     {
-        key_point_objects.push_back(std::make_shared<KeyPointObject>(kp,1));
+        key_point_objects.push_back(std::make_shared<keypoint_index>(kp, 1));
     }
 
-    auto item_set = MapSet(cv::Rect2d(0, 0, max_width, max_height), key_point_objects);
+    auto item_set = tianli::algorithms::container::tree(key_point_objects);
     item_set.print();
 
-    auto r =test_time(max_width, max_height, key_point_objects);
+    auto r = test_time(max_width, max_height, key_point_objects);
     std::cout << "time:" << r << " ms\n";
 }
