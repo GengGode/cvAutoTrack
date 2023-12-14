ea7a5a6a74ada27eb92115c583bf1ef8482b65e0
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-12-14 22:46:18 +0800

    style:统一命名风格

--- source/algorithms/feature/feature.create.h
+++ source/algorithms/feature/feature.create.h
@@ -4,13 +4,13 @@
 namespace tianli::algorithms::feature::create
 {
     // NEED_TEST: 构造均匀分布的特征点，基于网格划分，在每个网格内至少采样到目标密度的特征点
-    static features from_image(const cv::Mat& image, const cv::Mat& mask = cv::Mat(), const int target_density = 100)
+    static features from_image(const cv::Mat &image, const cv::Mat &mask = cv::Mat(), const int target_density = 100)
     {
-        const cv::Ptr<cv::xfeatures2d::SURF>& detector = cv::xfeatures2d::SURF::create();
-        const cv::Ptr<cv::xfeatures2d::SURF>& level_0_detector = cv::xfeatures2d::SURF::create(100, 1, 3, true, false);
-        const cv::Ptr<cv::xfeatures2d::SURF>& level_1_detector = cv::xfeatures2d::SURF::create(50, 1, 3, true, false);
-        const cv::Ptr<cv::xfeatures2d::SURF>& level_2_detector = cv::xfeatures2d::SURF::create(25, 1, 3, true, false);
-        const cv::Ptr<cv::xfeatures2d::SURF>& level_3_detector = cv::xfeatures2d::SURF::create(2, 1, 3, true, false);
+        const cv::Ptr<cv::xfeatures2d::SURF> &detector = cv::xfeatures2d::SURF::create();
+        const cv::Ptr<cv::xfeatures2d::SURF> &level_0_detector = cv::xfeatures2d::SURF::create(100, 1, 3, true, false);
+        const cv::Ptr<cv::xfeatures2d::SURF> &level_1_detector = cv::xfeatures2d::SURF::create(50, 1, 3, true, false);
+        const cv::Ptr<cv::xfeatures2d::SURF> &level_2_detector = cv::xfeatures2d::SURF::create(25, 1, 3, true, false);
+        const cv::Ptr<cv::xfeatures2d::SURF> &level_3_detector = cv::xfeatures2d::SURF::create(2, 1, 3, true, false);
         const float grid_radius = 512.0;
         // 将图像根据尺寸划分为网格
         const size_t grid_width = std::ceil(image.size[1] / grid_radius);
@@ -26,10 +26,10 @@ namespace tianli::algorithms::feature::create
 
         std::vector<features> res_features(grid_size);
 
-        //图像整体扩增边缘，以简化滑动窗口的计算
+        // 图像整体扩增边缘，以简化滑动窗口的计算
         int border_size = 32;
-        cv::Mat paddingImg;
-        cv::copyMakeBorder(image, paddingImg, border_size, border_size, border_size, border_size, cv::BORDER_REPLICATE);
+        cv::Mat padding_image;
+        cv::copyMakeBorder(image, padding_image, border_size, border_size, border_size, border_size, cv::BORDER_REPLICATE);
 
         for (int w_index = 0; w_index < grid_width; w_index++)
         {
@@ -47,10 +47,10 @@ namespace tianli::algorithms::feature::create
                 const cv::Rect grid_rect(grid_x, grid_y, grid_width + 2 * border_size, grid_height + 2 * border_size);
                 grid_rects[grid_index] = grid_rect;
                 // 计算网格中的特征点
-                cv::Mat grid_image = paddingImg(grid_rect);
+                cv::Mat grid_image = padding_image(grid_rect);
                 cv::Mat grid_mask;
                 if (!mask.empty())
-                    grid_mask = paddingImg(grid_rect);
+                    grid_mask = padding_image(grid_rect);
                 else
                     grid_mask = cv::Mat(grid_image.rows, grid_image.cols, CV_8UC1);
                 grid_features[grid_index] = feature::from_image(detector, grid_image, grid_mask);
@@ -87,9 +87,9 @@ namespace tianli::algorithms::feature::create
         features result_features;
         for (int i = 0; i < grid_size; i++)
         {
-            auto& grid_feature = res_features[i];
-            auto& grid_rect = grid_rects[i];
-            result_features = merge(result_features, grid_feature, cv::Point2d(grid_rect.x - border_size, grid_rect.y - border_size));
+            auto &grid_feature = res_features[i];
+            auto &grid_rect = grid_rects[i];
+            result_features = merge(result_features, grid_feature, cv::Point2f(grid_rect.x - border_size, grid_rect.y - border_size));
         }
         return result_features;
     }
