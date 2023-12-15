7ee3836ae281bdd1aa86479d69f7c6b74cdaf36f
Sallee <885331635@qq.com>
2023-12-15 22:05:46 +0800

    fix: 优化分块算法，尽量避免右边和下面出现面积过小的块 refactor: 优化分块算法命名

--- source/algorithms/feature/feature.create.h
+++ source/algorithms/feature/feature.create.h
@@ -5,40 +5,38 @@
 namespace tianli::algorithms::feature::create
 {
     // NEED_TEST: 构造均匀分布的特征点，基于网格划分，在每个网格内至少采样到目标密度的特征点
-    static features from_image(const cv::Mat& image, const int target_density = 100, const int grid_radius = 512, const cv::Mat& mask = cv::Mat())
+    static features from_image(const cv::Mat &image, const int target_density = 100, const int grid_radius = 512, const cv::Mat &mask = cv::Mat())
     {
         const float target_density_rate = 1.0 / (grid_radius * grid_radius) * target_density;
-        // 将图像根据尺寸划分为网格
-        const size_t grid_width = std::ceil(image.size[1] * 1.0 / grid_radius);
-        const size_t grid_height = std::ceil(image.size[0] * 1.0 / grid_radius);
-        const size_t grid_size = grid_width * grid_height;
-        // std::vector<cv::Rect> grid_rects(grid_size);
-        // std::vector<features> grid_features(grid_size);
+        // 将图像根据尺寸划分为网格，为了避免出现过小的块，宽高会减去一半边长，以实现将不足一半边长的块向前合并
+        const size_t grid_h_count = std::max(1, static_cast<int>(std::ceil((image.size[1] - grid_radius / 2) / grid_radius)));
+        const size_t grid_v_count = std::max(1, static_cast<int>(std::ceil((image.size[0] - grid_radius / 2) / grid_radius)));
+        const size_t grid_size = grid_h_count * grid_v_count;
+
         std::vector<std::pair<cv::Rect, features>> grid_features(grid_size);
         //  结果特征点
         features result_features;
-        // 特征点的生成算子
-        for (int w_index = 0; w_index < grid_width; w_index++)
+        for (int h_index = 0; h_index < grid_h_count; h_index++)
         {
-            for (int h_index = 0; h_index < grid_height; h_index++)
+            for (int v_index = 0; v_index < grid_v_count; v_index++)
             {
                 // 计算网格索引
-                const size_t grid_index = w_index * grid_height + h_index;
+                const size_t grid_index = h_index * grid_v_count + v_index;
                 // 计算网格范围
-                const size_t grid_x = w_index * grid_radius;
-                const size_t grid_y = h_index * grid_radius;
-                const size_t grid_width = grid_x + grid_radius > image.size[1] ? image.size[1] - grid_x : grid_radius;
-                const size_t grid_height = grid_y + grid_radius > image.size[0] ? image.size[0] - grid_y : grid_radius;
+                const size_t grid_x = h_index * grid_radius;
+                const size_t grid_y = v_index * grid_radius;
+                const size_t grid_width = h_index - 1 ? image.size[1] - grid_x : grid_radius;
+                const size_t grid_height = v_index - 1 ? image.size[0] - grid_y : grid_radius;
 
                 const cv::Rect grid_rect = cv::Rect(grid_x, grid_y, grid_width, grid_height);
                 grid_features[grid_index].first = grid_rect;
             }
         }
 
-        std::for_each(std::execution::par_unseq, grid_features.begin(), grid_features.end(), [&](std::pair<cv::Rect, features>& grid_feature)
+        std::for_each(std::execution::par_unseq, grid_features.begin(), grid_features.end(), [&](std::pair<cv::Rect, features> &grid_feature)
             {
                 const int fixed_target_density = std::ceil(target_density_rate * grid_feature.first.area());
-                const auto& detector = cv::xfeatures2d::SURF::create(100, 1, 3, true, false);
+                const auto &detector = cv::xfeatures2d::SURF::create(100, 1, 3, true, false);
                 float threshold = 100.0;
                 features feature;
                 for (int i = 0; i < 10; i++)
