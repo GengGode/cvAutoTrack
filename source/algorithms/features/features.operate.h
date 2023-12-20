2e1b317e0e2b7de087da7bc49946a1edb73abaf6
Yu SuiXian <genggode@outlook.com>
2023-12-20 00:55:47 +0000

    fix:修复重构features后导致的编译失败

--- source/algorithms/features/features.operate.h
+++ source/algorithms/features/features.operate.h
@@ -5,6 +5,73 @@
 
 namespace tianli::algorithms::features_operate
 {    
+    static features merge(const features &features_upper, const features &features_lower, const cv::Point2f diff_offset = cv::Point2f(0, 0))
+    {
+        features features_merge;
+        auto &[ids, kps, des] = features_merge;
+        ids = features_upper.indexs;
+        ids.push_back(features_upper.size());
+        for (auto i : features_lower.indexs)
+            ids.push_back(features_upper.size() + i);
+        kps = features_upper.keypoints;
+        std::for_each(features_lower.keypoints.begin(), features_lower.keypoints.end(), [&](const auto &kp)
+            { kps.emplace_back(kp.pt + diff_offset, kp.size, kp.angle, kp.response, kp.octave, kp.class_id); });
+        des = cv::Mat(features_upper.size() + features_lower.size(), 64, CV_16FC4);
+        des(cv::Rect(0, 0, 64, features_upper.size())) = features_upper.descriptors.clone();
+        des(cv::Rect(0, features_upper.size(), 64, features_lower.size())) = features_lower.descriptors.clone();
+        return features_merge;
+    }
+
+    static std::vector<features> split(const features &featuress)
+    {
+        if (featuress.indexs.size() == 0)
+            return { featuress };
+        auto &[ids, kps, des] = featuress;
+        std::vector<features> features_vec;
+        auto iter_size = featuress.size();
+        for (int rec_i = ids.size() - 1; rec_i >= 0; rec_i--)
+        {
+            int i = ids.at(rec_i);
+            auto end_features_size = iter_size - i;
+            auto begin = kps.cbegin() + i;
+            auto end = begin + end_features_size;
+            iter_size = iter_size - end_features_size;
+            features new_features;
+            new_features.keypoints = std::vector<cv::KeyPoint>(begin, end);
+            new_features.descriptors = des(cv::Rect(0, i, 64, end_features_size)).clone();
+            features_vec.insert(features_vec.begin(), new_features);
+        }
+        int i = 0;
+        auto end_features_size = iter_size - i;
+        auto begin = kps.cbegin() + i;
+        auto end = begin + end_features_size;
+        iter_size = iter_size - end_features_size;
+        features end_features;
+        end_features.keypoints = std::vector<cv::KeyPoint>(begin, end);
+        end_features.descriptors = des(cv::Rect(0, i, 64, end_features_size)).clone();
+        features_vec.insert(features_vec.begin(), end_features);
+        return features_vec;
+    }
+
+    static features join(const features &fts, std::vector<std::shared_ptr<point_index>> result)
+    {
+        features res;
+        res.keypoints = std::vector<cv::KeyPoint>(result.size());
+        res.descriptors = cv::Mat(result.size(), 64, CV_32F);
+        int index = 0;
+        for (const auto &item : result)
+        {
+            auto keypoint_ptr = std::dynamic_pointer_cast<keypoint_index>(item);
+            // 组合关键点
+            res.keypoints[index] = *keypoint_ptr->kp;
+            // 组合描述子
+            auto descriptor = fts.descriptors.row(keypoint_ptr->index);
+            descriptor.copyTo(res.descriptors.row(index));
+            index++;
+        }
+        return res;
+    }
+
     static features from_image(const cv::Ptr<cv::xfeatures2d::SURF> &detector, const cv::Mat &image, const cv::Mat &mask = cv::Mat())
     {
         const int border_size = 100;
@@ -81,7 +148,7 @@ namespace tianli::algorithms::features_operate
         // 将图像根据尺寸划分为网格，以均分为原则，尽可能接近给定的半径
         const size_t grids_row_count = static_cast<int>(std::ceil(1.0 * image.rows / grid_radius));
         const size_t grids_col_count = static_cast<int>(std::ceil(1.0 * image.cols / grid_radius));
-        const cv::Size2d grid_size(image.cols / grids_col_count, image.rows / grids_row_count);
+        const cv::Size2d grid_size(image.cols*1.0 / grids_col_count, image.rows*1.0 / grids_row_count);
         const size_t grid_count = grids_row_count * grids_col_count;
 
         std::vector<std::pair<cv::Rect, features>> grid_features(grid_count);
@@ -114,7 +181,7 @@ namespace tianli::algorithms::features_operate
                     if (threshold < 1 || feature.keypoints.size() > fixed_target_density)
                         break;
                     detector->setHessianThreshold(threshold);
-                    feature = feature::from_image(detector, image, grid_feature.first, mask);
+                    feature = from_image(detector, image, grid_feature.first, mask);
                     threshold /= 2.0;
                 }
                 grid_feature.second = feature;
@@ -123,72 +190,5 @@ namespace tianli::algorithms::features_operate
         for (int i = 0; i < grid_count; i++)
             result_features = merge(result_features, grid_features[i].second, cv::Point2d(grid_features[i].first.x, grid_features[i].first.y));
         return result_features;
-    }
-
-    static features merge(const features &features_upper, const features &features_lower, const cv::Point2f diff_offset = cv::Point2f(0, 0))
-    {
-        features features_merge;
-        auto &[ids, kps, des] = features_merge;
-        ids = features_upper.indexs;
-        ids.push_back(features_upper.size());
-        for (auto i : features_lower.indexs)
-            ids.push_back(features_upper.size() + i);
-        kps = features_upper.keypoints;
-        std::for_each(features_lower.keypoints.begin(), features_lower.keypoints.end(), [&](const auto &kp)
-            { kps.emplace_back(kp.pt + diff_offset, kp.size, kp.angle, kp.response, kp.octave, kp.class_id); });
-        des = cv::Mat(features_upper.size() + features_lower.size(), 64, CV_16FC4);
-        des(cv::Rect(0, 0, 64, features_upper.size())) = features_upper.descriptors.clone();
-        des(cv::Rect(0, features_upper.size(), 64, features_lower.size())) = features_lower.descriptors.clone();
-        return features_merge;
-    }
-
-    static std::vector<features> split(const features &featuress)
-    {
-        if (featuress.indexs.size() == 0)
-            return { featuress };
-        auto &[ids, kps, des] = featuress;
-        std::vector<features> features_vec;
-        auto iter_size = featuress.size();
-        for (int rec_i = ids.size() - 1; rec_i >= 0; rec_i--)
-        {
-            int i = ids.at(rec_i);
-            auto end_features_size = iter_size - i;
-            auto begin = kps.cbegin() + i;
-            auto end = begin + end_features_size;
-            iter_size = iter_size - end_features_size;
-            features new_features;
-            new_features.keypoints = std::vector<cv::KeyPoint>(begin, end);
-            new_features.descriptors = des(cv::Rect(0, i, 64, end_features_size)).clone();
-            features_vec.insert(features_vec.begin(), new_features);
-        }
-        int i = 0;
-        auto end_features_size = iter_size - i;
-        auto begin = kps.cbegin() + i;
-        auto end = begin + end_features_size;
-        iter_size = iter_size - end_features_size;
-        features end_features;
-        end_features.keypoints = std::vector<cv::KeyPoint>(begin, end);
-        end_features.descriptors = des(cv::Rect(0, i, 64, end_features_size)).clone();
-        features_vec.insert(features_vec.begin(), end_features);
-        return features_vec;
-    }
-
-    static features join(const features &fts, std::vector<std::shared_ptr<point_index>> result)
-    {
-        features res;
-        res.keypoints = std::vector<cv::KeyPoint>(result.size());
-        res.descriptors = cv::Mat(result.size(), 64, CV_32F);
-        int index = 0;
-        for (const auto &item : result)
-        {
-            auto keypoint_ptr = std::dynamic_pointer_cast<keypoint_index>(item);
-            // 组合关键点
-            res.keypoints[index] = *keypoint_ptr->kp;
-            // 组合描述子
-            auto descriptor = fts.descriptors.row(keypoint_ptr->index);
-            descriptor.copyTo(res.descriptors.row(index));
-            index++;
-        }
-        return res;
     }
 }
