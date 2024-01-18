7ab2f19ef1fa638a1c2cde26753dd45cbb003bb4
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-01-18 19:33:42 +0800

    update:切换到新错误码机制，仅msvc支持

--- source/module.version.h
+++ source/module.version.h
@@ -5,17 +5,21 @@
 #ifndef err
 #define err ErrorCode::getInstance()
 #endif
+#include <global/global.include.h>
+#define regerr(msg) err = {error(msg),utils::to_gbk(msg)}
+
 
 inline bool GetCoreCompileVersion(char *version_buff, int buff_size)
 {
 	if (version_buff == nullptr || buff_size < 1)
 	{
-		err = {291, utils::to_gbk("缓存区为空指针或是缓存区大小为小于1")};
+		regerr("缓存区为空指针或是缓存区大小为小于1");
 		return false;
 	}
 	if (TianLi::Version::build_version.size() > buff_size)
 	{
-		err = {292, utils::to_gbk("缓存区大小不足")};
+		//err("缓存区大小不足");
+		regerr("缓存区大小不足");
 		return false;
 	}
 	strcpy_s(version_buff, buff_size, TianLi::Version::build_version.c_str());
@@ -26,12 +30,14 @@ inline bool GetCoreCompileTime(char *time_buff, int buff_size)
 {
 	if (time_buff == nullptr || buff_size < 1)
 	{
-		err = {291, utils::to_gbk("缓存区为空指针或是缓存区大小为小于1")};
+		//err("缓存区为空指针或是缓存区大小为小于1");
+		regerr("缓存区为空指针或是缓存区大小为小于1");
 		return false;
 	}
 	if (TianLi::Version::build_time.size() > buff_size)
 	{
-		err = {292, utils::to_gbk("缓存区大小不足")};
+		//err("缓存区大小不足");
+		regerr("缓存区大小不足");
 		return false;
 	}
 	strcpy_s(time_buff, buff_size, TianLi::Version::build_time.c_str());
