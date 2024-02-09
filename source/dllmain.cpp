e73ee866f4ecdadce803c74747027da32ea6693e
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-02-09 10:10:56 +0800

    add:实现GetModulePath接口

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -1,8 +1,11 @@
 #include "pch.h"
 
-BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD ul_reason_for_call, LPVOID /*lpReserved*/
+static HMODULE g_library_module = NULL;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID /*lpReserved*/
 )
 {
+    g_library_module = hModule;
     switch (ul_reason_for_call)
     {
         case DLL_PROCESS_ATTACH:
@@ -11,4 +14,16 @@ BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD ul_reason_for_call, LPVOID /*lp
         case DLL_PROCESS_DETACH: break;
     }
     return TRUE;
+}
+
+bool GetModulePath(char* path_buff, int buff_size)
+{
+    if (g_library_module == NULL)
+    {
+        return false;
+    }
+    char module_path_char[MAX_PATH] = { 0 };
+    GetModuleFileNameA(g_library_module, module_path_char, MAX_PATH);
+    strcpy_s(path_buff, buff_size, module_path_char);
+    return true;
 }
