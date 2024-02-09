5c3f82159c9448d4e5722baf596b47114b361c7d
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-02-09 13:56:08 +0800

    Update release and publish core workflow and version numbers

--- ci/upload_core_file.ps1
+++ ci/upload_core_file.ps1
@@ -1,18 +1,18 @@
 #$version = '7.9.5-6-g6c9f02b'
+#$login_password = ''
 
 $successTime = 0
 
 $login_username = 'GitAction'
-$login_password = 'iOpEv21kPOt5ZrMqViLKvJJXshh6olNR'
 $login_api = '/api/auth/login'
-$login_url = 'http://download.weixitianli.com' + $login_api + '?Username=' + $login_username + '&Password=' + $login_password
+$login_url = 'https://download.weixitianli.com' + $login_api + '?Username=' + $login_username + '&Password=' + $login_password
 $token = (Invoke-RestMethod -Uri $login_url -Method Post).data.token
 Write-Host $token
 
 Write-Host Uploading
 
 $upload_api = '/api/fs/put'
-$upload_url = 'http://download.weixitianli.com' + $upload_api
+$upload_url = 'https://download.weixitianli.com' + $upload_api
 $upload_file = 'cvAutoTrack-' + $version + '.zip'
 $upload_file_length = (Get-Item $upload_file).Length
 $upload_paths = @('/sync/TianLiUpdateService.tianyi/cvAutoTrack.Core/',
@@ -24,7 +24,7 @@ foreach($upload_path in $upload_paths) {
     Authorization    = $token
     'Content-Type'   = 'application/x-zip-compressed'
     'Content-Length' = $upload_file_length
-    'file-path'      = $upload_path+$upload_file
+    'file-path'      = $upload_path + $upload_file
   }
 
   try {
